9dec0d5b07418b2abbe4c7ac431249bd83a25bf2
Loic Matthey <loic.matthey@vtxnet.ch>
2011-08-10 15:01:56 +0100

    New function with direct bounds for circular data. More tested, seems to work. Maybe not great for consistency...

--- slicesampler.py
+++ slicesampler.py
@@ -10,14 +10,15 @@ Copyright (c) 2011 Gatsby Unit. All rights reserved.
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import scipy.special as scsp
 
 class SliceSampler:
-    def __init__(self, step_out=True):
-        self.step_out = step_out
+    def __init__(self):
+        pass
         
     
     
-    def sample(self, N, x_initial, loglike_fct, burn=100, widths=1., last_loglikehood=None, loglike_fct_params=None):
+    def sample(self, N, x_initial, loglike_fct, burn=100, widths=1., last_loglikehood=None, loglike_fct_params=None, step_out=True):
         
         '''
             Simple axis-aligned implementation of slice sampling for vectors
@@ -71,7 +72,7 @@ class SliceSampler:
                 x_r[d] += (1.-rr)*widths[d]
                 
                 # Adapt the interval, if the width could be too small (speedup: remove that)
-                if self.step_out:
+                if step_out:
                     llh_l = loglike_fct(x_l, loglike_fct_params)
                     while llh_l > log_uprime:
                         x_l[d] = x_l[d] - widths[d]
@@ -103,7 +104,7 @@ class SliceSampler:
         return samples, last_loglikehood
     
     
-    def sample_1D(self, N, x_initial, loglike_fct, burn=100, widths=1., last_loglikehood=None, loglike_fct_params=None, bounds=(-np.inf, np.inf), thinning=1):
+    def sample_1D(self, N, x_initial, loglike_fct, burn=100, widths=1., last_loglikehood=None, loglike_fct_params=None, step_out = True, bounds=(-np.inf, np.inf), thinning=1, debug=False):
         
         '''
             Simple implementation of slice sampling for 1D variable
@@ -127,15 +128,16 @@ class SliceSampler:
         
         
         # Initialisation
-        samples = np.zeros(N/thinning)
+        samples = np.zeros(N)
         
         if last_loglikehood is None:
             last_loglikehood = loglike_fct(x_initial, loglike_fct_params)
         
         x_new  = x_initial
+        j = 0
         
         # N samples
-        for i in np.arange(N+burn):
+        for i in np.arange(thinning*N+burn, dtype='int32'):
             log_uprime  = last_loglikehood + np.log(np.random.rand())
             # print log_uprime
             
@@ -145,7 +147,7 @@ class SliceSampler:
             x_r    = x_new + (1.-rr)*widths
             
             # Grow the interval to get an unbiased slice
-            if self.step_out:
+            if step_out:
                 llh_l = loglike_fct(x_l, loglike_fct_params)
                 while llh_l > log_uprime:
                     x_l     -= widths
@@ -178,7 +180,107 @@ class SliceSampler:
             
             # Store this sample
             if i >= burn and (i % thinning == 0):
-                samples[i-burn] = x_new
+                if debug:
+                    print "Sample %d" % (j+1)
+                samples[j] = x_new
+                j += 1
+        
+        return samples, last_loglikehood
+    
+    
+    def sample_1D_circular(self, N, x_initial, loglike_fct, burn=100, widths=1., last_loglikehood=None, loglike_fct_params=None, step_out = True, thinning=1, debug=False, loglike_min=-np.inf):
+        
+        '''
+            Simple implementation of slice sampling for 1D variable
+            
+            Inputs:
+                N                   1x1     Number of samples to gather
+                x_initial           1x1     initial state
+                loglike_fct         @fn     function logprobstar = logdist(x, loglike_fct_params)
+                burn                1x1     after burning period of this length
+                widths              1x1     step sizes for slice sampling. Should correspond.
+                last_loglikehood    1x1     precomputed last loglikehood
+                step_out            bool    set to true if widths may sometimes be far too small
+                loglike_fct_params  dict    any extra arguments are passed on to logdist
+            
+            Outputs:
+                samples  Nx1   samples
+                
+                Iain Murray May 2004, tweaks June 2009, a diagnostic added Feb 2010
+                See Pseudo-code in David MacKay's text book p375
+        '''
+        
+        
+        # Initialisation
+        samples = np.zeros(N)
+        
+        if last_loglikehood is None:
+            last_loglikehood = loglike_fct(x_initial, loglike_fct_params)
+        
+        x_new  = x_initial
+        j = 0
+        
+        # N samples
+        for i in np.arange(thinning*N+burn, dtype='int32'):
+            log_uprime  = last_loglikehood + np.log(np.random.rand())
+            # print "log_uprime: %.3f, lastllh: %.3f" % (log_uprime, last_loglikehood)
+            
+            # Create a horizontal interval (x_l, x_r) enclosing x_new. Place it randomly.
+            rr     = np.random.rand()
+            x_l    = x_new -  rr*widths
+            x_r    = x_new + (1.-rr)*widths
+            
+            # Grow the interval to get an unbiased slice
+            if step_out:
+                if log_uprime < loglike_min:
+                    # The current slice is too small for the likelihood, step_out will just hit the bounds.
+                    x_l = -np.pi
+                    x_r = np.pi
+                else:
+                    llh_l = loglike_fct(x_l, loglike_fct_params)
+                    # s = 0
+                    while llh_l > log_uprime:
+                        # print "stepping out left: [%.3f < %.3f < %.3f] %.3f %.3f" % (x_l, x_new, x_r, log_uprime, llh_l)
+                        x_l     -= widths
+                        llh_l   = loglike_fct(x_l, loglike_fct_params)
+                        if x_l <= -np.pi:
+                            x_l = -np.pi
+                            break
+                        
+                        # s+=1
+                    # print s
+                    
+                    llh_r = loglike_fct(x_r, loglike_fct_params)
+                    while llh_r > log_uprime:
+                        x_r     += widths
+                        llh_r   = loglike_fct(x_r, loglike_fct_params)
+                        if x_r >= np.pi:
+                            x_r = np.pi
+                            break
+                
+            
+            # Sample a new point, shrinking the interval
+            while True:
+                xprime = np.random.rand()*(x_r - x_l) + x_l
+                
+                last_loglikehood = loglike_fct(xprime, loglike_fct_params)
+                if last_loglikehood > log_uprime:
+                    # Accept this sample
+                    x_new = xprime
+                    break
+                elif xprime > x_new:
+                    x_r  = x_new
+                elif xprime < x_new:
+                    x_l = x_new
+                else:
+                    raise RuntimeError("Slice sampler shrank too far.")
+            
+            # Store this sample
+            if i >= burn and (i % thinning == 0):
+                if debug:
+                    print "Sample %d: %.3f" % (j+1, x_new)
+                samples[j] = x_new
+                j += 1
         
         return samples, last_loglikehood
 
@@ -186,14 +288,14 @@ class SliceSampler:
 
 if __name__ == '__main__':
     
-    if True:
+    if False:
         # Try it with a gaussian
         loglike_fct = lambda x, p=[0.0, 1.0]: (-0.5/p[1]**2.)*((x-p[0])**2.)
         loglike_fct_params = [0.0, 1.0]
         
         # Get samples
-        slicesampler = SliceSampler(step_out = True)
-        samples, last_llh = slicesampler.sample_1D(50000, -1000., loglike_fct, burn=100, widths=1.0, loglike_fct_params=loglike_fct_params)
+        slicesampler = SliceSampler()
+        samples, last_llh = slicesampler.sample_1D(50000, np.random.rand(), loglike_fct, burn=100, widths=0.2, loglike_fct_params=loglike_fct_params, step_out=True)
         
         
         # Plot the results
@@ -211,24 +313,28 @@ if __name__ == '__main__':
         print "Original parameters: %f, %f" % (loglike_fct_params[0], loglike_fct_params[1])
         print "Fitted parameters: %f, %f" % (np.mean(samples), np.cov(samples))
     
-    if False:
+    if True:
         # Try with Von Mises
-        loglike_theta_fct = lambda x, (mu, kappa): kappa*np.cos(x - mu)
-        loglike_fct_params = [0.0, 0.01]
+        loglike_theta_fct = lambda x, (mu, kappa): kappa*np.cos(x - mu) - np.log(2.*np.pi) - np.log(scsp.i0(kappa))
+        loglike_fct_params = [0.0, 0.1]
         
         # Get samples
-        slicesampler = SliceSampler(step_out = True)
-        samples2, last_llh = slicesampler.sample_1D(50000, np.random.rand(), loglike_theta_fct, burn=100, widths=1.0, loglike_fct_params=loglike_fct_params, bounds=(-np.pi, np.pi))
+        slicesampler = SliceSampler()
+        samples2, last_llh = slicesampler.sample_1D_circular(50000, np.random.rand(), loglike_theta_fct, burn=500, widths=0.01, loglike_fct_params=loglike_fct_params, step_out=True, debug=True, loglike_min = -np.log((2./2.0)*np.pi*scsp.i0(loglike_fct_params[1])))
+        # samples2, last_llh = slicesampler.sample_1D_circular(50000, np.random.rand(), loglike_theta_fct, burn=500, widths=0.01, loglike_fct_params=loglike_fct_params, step_out=True, debug=True)
         
         # Plot
         x = np.linspace(-4., 4., 100)
         n, left_x = np.histogram(samples2, bins=x)
         
         like_out = np.exp(loglike_theta_fct(x, loglike_fct_params))
-        like_out /= np.sum(like_out)
+        like_out /= np.abs(np.max(like_out))
+        # like_out -= np.mean(like_out)
         
         plt.figure()
-        plt.bar(x[:-1], n/np.sum(n.astype(float)), facecolor='green', alpha=0.75, width=np.diff(x)[0])
+        plt.bar(x[:-1], n/np.max(n.astype(float)), facecolor='green', alpha=0.75, width=np.diff(x)[0])
         plt.plot(x, like_out, 'r')
+    
+    
     
     
