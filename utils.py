0bda7a6f87e57dbe10e718babff5852c7f87b367
Loic Matthey <loic.matthey@vtxnet.ch>
2012-07-06 13:07:15 +0100

    Cleanup and function to plot histogram of angular values
    - Big cleanup, added comments to separate pieces of code. Should be refactored further, maybe create a class for plotting libraries for example…
    - Added a helper function to create a histogram of angular values. Nice convenenience function.

--- utils.py
+++ utils.py
@@ -20,17 +20,48 @@ from matplotlib.ticker import LinearLocator
 
 __maxexp__ = np.finfo('float').maxexp
 
-def cross(*args):
-    ans = [[]]
-    for arg in args:
-        if isinstance(arg[0], list) or isinstance(arg[0], tuple):
-            for a in arg:
-                ans = [x+[y] for x in ans for y in a]
-        else:
-            ans = [x+[y] for x in ans for y in arg]
-    return ans
 
-def lbtoangle(b1,l1,b2,l2):
+############################ MATH ##################################
+
+def powerlaw(x, amp, index):
+    '''
+        Computes the powerlaw:
+        y = amp * (x**index)
+    '''
+
+    return amp * (x**index)
+
+############################## DIRECTIONAL STATISTICS ################################
+
+def mean_angles(angles):
+    return np.angle(np.mean(np.exp(1j*angles), axis=0))
+
+
+def wrap_angles(angles, bound=np.pi):
+    '''
+        Wrap angles in a [-bound, bound] space.
+
+        For us: get the smallest angle between two responses
+    '''
+
+    # if np.isscalar(angles):
+    #     while angles < -bound:
+    #         angles += 2.*bound
+    #     while angles > bound:
+    #         angles -= 2.*bound
+    # else:
+    #     while np.any(angles < -bound):
+    #         angles[angles < -bound] += 2.*bound
+    #     while np.any(angles > bound):
+    #         angles[angles > bound] -= 2.*bound
+    
+    angles = np.mod(angles + bound, 2*bound) - bound
+
+    return angles
+
+############################ SPHERICAL/3D COORDINATES ##################################
+
+def lbtoangle(b1, l1, b2, l2):
     if b1 < 0.:
         b1 = 360.+b1
     if l1 < 0.:
@@ -78,9 +109,9 @@ def dist_sphere_mat(points1, points2):
     # points2_pos[points2_pos<0.0] += 2.*np.pi
 
     p12 = np.cos(points1_pos - points2_pos)
-    p3 = np.cos(points1_pos + points2_pos)[:,0]
+    p3 = np.cos(points1_pos + points2_pos)[:, 0]
 
-    return np.arccos((p12[:,1]*(p12[:,0]+p3) + p12[:,0]-p3)/2.)
+    return np.arccos((p12[:, 1]*(p12[:, 0]+p3) + p12[:, 0]-p3)/2.)
 
 def spherical_to_vect(angles):
     output_vect = np.zeros(3)
@@ -118,8 +149,34 @@ def gs_ortho(input_vect, ortho_target):
     output = input_vect - np.dot(ortho_target, input_vect)*ortho_target
     return output/np.linalg.norm(output)
 
+
+########################## TRICKS AND HELPER FUNCTIONS #################################
+
 def flatten_list(ll):
     return [item for sublist in ll for item in sublist]
+
+
+def list_2_tuple(arg):
+    if (isinstance(arg, list) and len(arg) > 0 and isinstance(arg[0], list)):
+        a = tuple([list_2_tuple(x) for x in arg])
+    else:
+        a = tuple(arg)
+    
+    return a
+
+def cross(*args):
+    ans = [[]]
+    for arg in args:
+        if isinstance(arg[0], list) or isinstance(arg[0], tuple):
+            for a in arg:
+                ans = [x+[y] for x in ans for y in a]
+        else:
+            ans = [x+[y] for x in ans for y in arg]
+    return ans
+
+def strcat(*strings):
+    return ''.join(strings)
+
 
 def fast_dot_1D(x, y):
     out = 0
@@ -129,6 +186,51 @@ def fast_dot_1D(x, y):
 
 def fast_1d_norm(x):
     return np.sqrt(np.dot(x, x.conj()))
+
+def array2string(array):
+    # return np.array2string(array, suppress_small=True)
+    if array.ndim == 2:
+        return '  |  '.join([' '.join(str(k) for k in item) for item in array])
+    elif array.ndim == 3:
+        return '  |  '.join([', '.join([' '.join([str(it) for it in obj]) for obj in item]) for item in array])
+
+def argmin_indices(array):
+    return np.unravel_index(np.nanargmin(array), array.shape)
+
+def argmax_indices(array):
+    return np.unravel_index(np.nanargmax(array), array.shape)
+
+
+########################## FILE I/O #################################
+
+def unique_filename(prefix=None, suffix=None, unique_id=None, return_id=False):
+    """
+    Get an unique filename with uuid4 random strings
+    """
+    fn = []
+    if prefix:
+        fn.extend([prefix, '-'])
+    
+    if unique_id is None:
+        unique_id = str(uuid.uuid4())
+    
+    fn.append(unique_id)
+    
+    if suffix:
+        fn.extend(['.', suffix.lstrip('.')])
+    
+    if return_id:
+        return [''.join(fn), unique_id]
+    else:
+        return ''.join(fn)
+
+def numpy_2_mat(array, filename, arrayname):
+    sio.savemat('%s.mat' % filename, {'%s' % arrayname: array})
+
+
+
+########################## PLOTTING FUNCTIONS #################################
+
 
 def plot_mean_std_area(x, y, std, ax_handle=None):
     if ax_handle is None:
@@ -161,17 +263,6 @@ def semilogy_mean_std_area(x, y, std, ax_handle=None):
     return ax_handle
 
 
-def strcat(*strings):
-    return ''.join(strings)
-
-
-def array2string(array):
-    # return np.array2string(array, suppress_small=True)
-    if array.ndim == 2:
-        return '  |  '.join([' '.join(str(k) for k in item) for item in array])
-    elif array.ndim == 3:
-        return '  |  '.join([', '.join([' '.join([str(it) for it in obj]) for obj in item]) for item in array])
-
 def plot_square_grid(x, y, nb_to_plot=-1):
     '''
         Construct a square grid of plots
@@ -187,13 +278,39 @@ def plot_square_grid(x, y, nb_to_plot=-1):
     for i in np.arange(nb_plots_sqrt):
         for j in np.arange(nb_plots_sqrt):
             try:
-                subaxes[i,j].plot(x[nb_plots_sqrt*i+j], y[nb_plots_sqrt*i+j])
-                subaxes[i,j].xaxis.set_major_locator(plttic.NullLocator())
-                subaxes[i,j].yaxis.set_major_locator(plttic.NullLocator())
+                subaxes[i, j].plot(x[nb_plots_sqrt*i+j], y[nb_plots_sqrt*i+j])
+                subaxes[i, j].xaxis.set_major_locator(plttic.NullLocator())
+                subaxes[i, j].yaxis.set_major_locator(plttic.NullLocator())
             except IndexError:
-                subaxes[i,j].set_visible(False)
+                subaxes[i, j].set_visible(False)
     
     return (f, subaxes)    
+
+
+def histogram_angular_data(data, bins=20, in_degrees=False, title=None, norm=None):
+
+    if in_degrees:
+        bound_x = 180.
+        data *= 180./np.pi
+    else:
+        bound_x = np.pi
+    
+    x = np.linspace(-bound_x, bound_x, bins)
+    x_edges = x - bound_x/bins  # np.histogram wants the left-right boundaries...
+    x_edges = np.r_[x_edges, -x_edges[0]]  # the rightmost boundary is the mirror of the leftmost one
+    
+    bar_heights, _ = np.histogram(data, bins=x_edges)
+
+    if norm == 'max':
+        bar_heights = bar_heights/np.max(bar_heights).astype('float')
+    elif norm == 'sum':
+        bar_heights = bar_heights/np.sum(bar_heights.astype('float'))
+    
+    plt.figure()
+    plt.bar(x, bar_heights, alpha=0.75, width=2.*bound_x/(bins-1), align='center')
+    if title:
+        plt.title(title)
+    plt.xlim([x[0]*1.1, 1.1*x[-1]])
 
 
 def pcolor_square_grid(data, nb_to_plot=-1):
@@ -211,14 +328,15 @@ def pcolor_square_grid(data, nb_to_plot=-1):
     for i in np.arange(nb_plots_sqrt):
         for j in np.arange(nb_plots_sqrt):
             try:
-                subaxes[i,j].imshow(data[nb_plots_sqrt*i+j], interpolation='nearest')
-                subaxes[i,j].xaxis.set_major_locator(plttic.NullLocator())
-                subaxes[i,j].yaxis.set_major_locator(plttic.NullLocator())
+                subaxes[i, j].imshow(data[nb_plots_sqrt*i+j], interpolation='nearest')
+                subaxes[i, j].xaxis.set_major_locator(plttic.NullLocator())
+                subaxes[i, j].yaxis.set_major_locator(plttic.NullLocator())
             except IndexError:
-                subaxes[i,j].set_visible(False)
+                subaxes[i, j].set_visible(False)
                 
     return (f, subaxes)
     
+
 def plot_sphere(theta, gamma, Z, weight_deform=0.5, sphere_radius=1., try_mayavi=True):
     '''
         Plot a sphere, with the color set by Z.
@@ -245,7 +363,7 @@ def plot_sphere(theta, gamma, Z, weight_deform=0.5, sphere_radius=1., try_mayavi
         
     if use_mayavi:
         mplt.figure()
-        mplt.mesh(x,y,z, scalars=Z_norm)
+        mplt.mesh(x, y, z, scalars=Z_norm)
         mplt.show()
 
     else:
@@ -260,6 +378,7 @@ def plot_sphere(theta, gamma, Z, weight_deform=0.5, sphere_radius=1., try_mayavi
         plt.colorbar(m)
 
         plt.show()
+
 
 def plot_torus(theta, gamma, Z, weight_deform=0., torus_radius=5., tube_radius=3.0, try_mayavi=True, draw_colorbar=True):
     '''
@@ -289,12 +408,12 @@ def plot_torus(theta, gamma, Z, weight_deform=0., torus_radius=5., tube_radius=3
     if use_mayavi:
         # mplt.figure(bgcolor=(0.7,0.7,0.7))
         mplt.figure(bgcolor=(1.0, 1.0, 1.0))
-        mplt.mesh(x,y,z, scalars=Z_norm, vmin=0.0)
+        mplt.mesh(x, y, z, scalars=Z_norm, vmin=0.0)
         
         if draw_colorbar:
             cb = mplt.colorbar(title='', orientation='vertical', label_fmt='%.2f', nb_labels=5)
         
-        mplt.outline(color=(0.,0.,0.))
+        mplt.outline(color=(0., 0., 0.))
         mplt.show()
 
     else:
@@ -313,84 +432,39 @@ def plot_torus(theta, gamma, Z, weight_deform=0., torus_radius=5., tube_radius=3
         plt.show()
 
 
-def argmin_indices(array):
-    return np.unravel_index(np.nanargmin(array), array.shape)
-
-def argmax_indices(array):
-    return np.unravel_index(np.nanargmax(array), array.shape)
-
-def mean_angles(angles):
-    return np.angle(np.mean(np.exp(1j*angles), axis=0))
-
-def wrap_angles(angles, bound=np.pi):
+def plot_powerlaw_fit(xdata, ydata, amp, index):
     '''
-        Wrap angles in a [-bound, bound] space.
-
-        For us: get the smallest angle between two responses
+        Plot a powerlaw with some associated datapoints
     '''
 
-    if np.isscalar(angles):
-        while angles < -bound:
-            angles += 2.*bound
-        while angles > bound:
-            angles -= 2.*bound
-    else:
-        while np.any(angles < -bound):
-            angles[angles < -bound] += 2.*bound
-        while np.any(angles > bound):
-            angles[angles > bound] -= 2.*bound
-            
-    return angles
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.plot(xdata, powerlaw(xdata, amp, index))     # Fit
+    plt.plot(xdata, ydata, 'k.')  # Data
+    # plt.text(0.0, 0.0, 'Ampli = %5.2f +/- %5.2f' % (amp, ampErr))
+    # plt.text(0.0, 0.0, 'Index = %5.2f +/- %5.2f' % (index, indexErr))
+    
+    plt.title('Best Fit Power Law')
+    plt.xlabel('X')
+    plt.ylabel('Y')
 
-def unique_filename(prefix=None, suffix=None, unique_id=None, return_id=False):
-    """
-    Get an unique filename with uuid4 random strings
-    """
-    fn = []
-    if prefix:
-        fn.extend([prefix, '-'])
-    
-    if unique_id is None:
-        unique_id = str(uuid.uuid4())
-    
-    fn.append(unique_id)
-    
-    if suffix:
-        fn.extend(['.', suffix.lstrip('.')])
-    
-    if return_id:
-        return [''.join(fn), unique_id]
-    else:
-        return ''.join(fn)
+    plt.subplot(2, 1, 2)
+    plt.loglog(xdata, powerlaw(xdata, amp, index))
+    plt.plot(xdata, ydata, 'k.')  # Data
+    plt.xlabel('X (log scale)')
+    plt.ylabel('Y (log scale)')
 
 
-def list_2_tuple(arg):
-    if (isinstance(arg, list) and len(arg) > 0 and isinstance(arg[0], list)):
-        a = tuple([list_2_tuple(x) for x in arg])
-    else:
-        a = tuple(arg)
-    
-    return a
 
-
-def numpy_2_mat(array, filename, arrayname):
-    sio.savemat('%s.mat' % filename, {'%s' % arrayname: array})
-
-def reinstantiate_variables_dict(var_dict):
-    '''
-        TO BE COPIED
-        doesn't work like that, whatever.
-    '''
-    for k, x in var_dict.items():
-        vars()[k] = x
-    
+################################# FITTING ########################################
 
 def fit_powerlaw(xdata, ydata, should_plot=False, debug=False):
     '''
-        Fit a power law to the provided data
+        Fit a power law to the provided data.
+        Actually fit to the mean of the provided data, if multiple columns given (axis 1)
+
+        Look for next function in order to fit a powerlaw while taking std dev into account.
     '''
-    
-    powerlaw = lambda x, amp, index: amp * (x**index)
 
     ##########
     # Fitting the data -- Least Squares Method
@@ -403,25 +477,31 @@ def fit_powerlaw(xdata, ydata, should_plot=False, debug=False):
     #  log(y) = log(a) + b*log(x)
     #
 
-    logx = np.log(xdata)
+    if ydata.ndim == 1:
+        # We have a 1D array, need a flat 2D array...
+        ydata = ydata[:, np.newaxis]
+
+    if xdata.ndim == 1:
+        # We need to tile the x values appropriately, to fit the size of y
+        # (just because leastsq is not funny)
+        xdata = np.tile(xdata, (ydata.shape[1], 1)).T
+
+    logx = np.log(xdata.astype('float'))
     logy = np.log(ydata)
 
     # define our (line) fitting function
     fitfunc = lambda p, x: p[0] + p[1] * x
-    errfunc = lambda p, x, y: (y - fitfunc(p, x))
+    errfunc = lambda p, x, y: np.mean(y - fitfunc(p, x), axis=1)
+    # errfunc = lambda p, x, y: (y - fitfunc(p, x))
     # errfunc_mse = lambda p, x, y: np.mean((y - fitfunc(p, x))**2.)
 
     # Initial parameters
-    pinit = [1.0, -1.0]
+    pinit = np.array([1.0, -1.0])
     out = spopt.leastsq(errfunc, pinit, args=(logx, logy), full_output=1)
     # out = spopt.fmin(errfunc_mse, pinit, args=(logx, logy))
 
     pfinal = out[0]
     covar = out[1]
-
-    if debug:
-        print pfinal
-        print covar
 
     index = pfinal[1]
     amp = np.exp(pfinal[0])
@@ -429,28 +509,18 @@ def fit_powerlaw(xdata, ydata, should_plot=False, debug=False):
     indexErr = np.sqrt( covar[0][0] )
     ampErr = np.sqrt( covar[1][1] ) * amp
 
+    if debug:
+        print pfinal
+        print covar
+        print 'Ampli = %5.2f +/- %5.2f' % (amp, ampErr)
+        print 'Index = %5.2f +/- %5.2f' % (index, indexErr)
+    
     ##########
     # Plotting data
     ##########
     if should_plot:
-        plt.figure()
-        plt.subplot(2, 1, 1)
-        plt.plot(xdata, powerlaw(xdata, amp, index))     # Fit
-        plt.plot(xdata, ydata, 'k.')  # Data
-        # plt.text(0.0, 0.0, 'Ampli = %5.2f +/- %5.2f' % (amp, ampErr))
-        # plt.text(0.0, 0.0, 'Index = %5.2f +/- %5.2f' % (index, indexErr))
-        if debug:
-            print 'Ampli = %5.2f +/- %5.2f' % (amp, ampErr)
-            print 'Index = %5.2f +/- %5.2f' % (index, indexErr)
-        plt.title('Best Fit Power Law')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-
-        plt.subplot(2, 1, 2)
-        plt.loglog(xdata, powerlaw(xdata, amp, index))
-        plt.plot(xdata, ydata, 'k.')  # Data
-        plt.xlabel('X (log scale)')
-        plt.ylabel('Y (log scale)')
+        plot_powerlaw_fit(xdata, ydata, amp, index)
+                
 
     return np.array([index, amp])
         
