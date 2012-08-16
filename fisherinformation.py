1541471beebb38b9ca9016063a5c05e77f44f592
Loic Matthey <loic.matthey@vtxnet.ch>
2012-08-16 14:17:07 +0100

    Effect of normalised receptive fields, use computed KL covariance
    - Add: Now with normalised receptive fields. Get a square dependence on kappa. Still, the sampler seems to get a linear dependence on kappa. Weird.
    - Add: Uses computed KL covariance now. Same results, super faster.
    - Add: redid all computations, checking for discrepancies… Nothing weird.
    
    work on #10 and #17

--- fisherinformation.py
+++ fisherinformation.py
@@ -176,11 +176,11 @@ if __name__ == '__main__':
         plt.figure()
         plt.plot(T_all, all_FI_11_T)
 
-    if True:
+    if False:
         # Do the optimisation
         target_experimental_precisions  = np.array([5.0391, 3.4834, 2.9056, 2.2412, 1.7729])/2.
 
-        N_sqrt = 20.
+        N_sqrt = int(200.**0.5)
 
         # sigma_x_2 = (0.5)**2.
         kappa1 = 0.8
@@ -196,7 +196,7 @@ if __name__ == '__main__':
         sigma_y = 0.001
 
         precision = 1.0
-        stim_space = np.linspace(0, 2.*np.pi, precision, endpoint=False)
+        stim_space = np.linspace(-np.pi, np.pi, precision, endpoint=False)
         stim_space = np.array(cross(stim_space, stim_space))
 
         T_all = np.arange(1, 6)
@@ -216,7 +216,7 @@ if __name__ == '__main__':
 
             # Full FI
             for T_i, T in enumerate(T_all):
-                FI_T[T_i] = kappa1**2.*np.mean(np.sum(np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis]))*np.linalg.solve(covariances_all[T_i], np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis]))), axis=0))
+                FI_T[T_i] = (kappa1**2./(sigma_x**2.*16.*np.pi**4.0*scsp.i0(kappa1)**2.0*scsp.i0(kappa2)**2.0)*np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis]))*np.linalg.solve(covariances_all[T_i], np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis])))).mean(axis=-1).sum(axis=-1)
                 # all_FI_11_Tt[i, j] = alpha**(2.*T-2.*t + 2.)*1./sigma_x_2 * kappa1**2.*np.mean(np.sum(np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis]))*np.sin(stim_space[:, 0] - mu[:, np.newaxis])*np.exp(kappa1*np.cos(stim_space[:, 0] - mu[:, np.newaxis]) + kappa2*np.cos(stim_space[:, 1] - gamma[:, np.newaxis])), axis=0))
 
                 # FI_Tt[i, j] = alpha**(2.*T-2.*t)*1./sigma_x**2.*density*2.*np.pi**2.*kappa1**2.0*scsp.i0(2*kappa2)*(scsp.i0(2*kappa1) - scsp.iv(2, 2*kappa1))
@@ -286,26 +286,45 @@ if __name__ == '__main__':
             plt.semilogy(sigma_x_space, sigma_x_powerlaw_params[:, 1])
 
         if True:
-            sigma_x = 1.0
+            sigma_x = 0.2
+            beta = 1.0
+            sigma_y = 0.01
+            
+            N_sqrt = 17.0
+
+            N = int(N_sqrt**2.)
+
+            coverage_1D = np.linspace( -np.pi + np.pi/N_sqrt, np.pi + np.pi/N_sqrt, N_sqrt, endpoint=False)
+            means = np.array(cross(2*[coverage_1D.tolist()]))
+            mu = means[:, 0]
+            gamma = means[:, 1]
 
             #### See effect of kappa
-            kappa_space = np.linspace(0.05, 30.0, 100.)
+            kappa_space = np.linspace(0.05, 15.0, 10.)
+            # kappa_space = np.array([1.0])
+
+            T_all = np.arange(1, 2)
 
             kappa_MSE_experimental = np.zeros_like(kappa_space)
             FI_all_kappa = np.zeros((kappa_space.size, T_all.size))
             kappa_powerlaw_params = np.zeros((kappa_space.size, 2))
 
-            covariances_all = np.zeros((T_all.size, int(N_sqrt**2.), int(N_sqrt**2.)))
+            covariances_all = np.zeros((T_all.size, N, N))
 
             for i, kappa in enumerate(kappa_space):
                 print "%.f%%, Doing kappa: %.2f" % (i*100./kappa_space.size, kappa)
                 
                 for T_i, T in enumerate(T_all):
-                    time_weights_parameters = dict(weighting_alpha=1.0, weighting_beta=1.0, specific_weighting=0.1, weight_prior='uniform')
-                    rn = RandomFactorialNetwork.create_full_conjunctive(int(N_sqrt**2.), R=2, scale_moments=(kappa, 0.0001), ratio_moments=(1.0, 0.0001))
-                    data_gen_noise = DataGeneratorRFN(3000, T, rn, sigma_y=sigma_y, time_weights_parameters=time_weights_parameters, cued_feature_time=T-1)
-                    stat_meas = StatisticsMeasurer(data_gen_noise)
-                    covariances_all[T_i] = stat_meas.model_parameters['covariances'][2][-1]
+                    time_weights_parameters = dict(weighting_alpha=1.0, weighting_beta=beta, specific_weighting=0.1, weight_prior='uniform')
+                    rn = RandomFactorialNetwork.create_full_conjunctive(N, R=2, scale_moments=(kappa**2.0, 0.0001), ratio_moments=(1.0, 0.0001))
+                    # Measure covariances
+                    # data_gen_noise = DataGeneratorRFN(3000, T, rn, sigma_x=sigma_x, sigma_y=sigma_y, time_weights_parameters=time_weights_parameters, cued_feature_time=T-1)
+                    # stat_meas = StatisticsMeasurer(data_gen_noise)
+                    # covariances_all[T_i] = stat_meas.model_parameters['covariances'][2][-1]
+
+                    # Computed covariances
+                    covariances_all[T_i] = rn.compute_covariance_KL(sigma_2=(beta**2.0*sigma_x**2. + sigma_y**2.), T=T, beta=beta, precision=20)
+                    # covariances_all[T_i] = sigma_x**2.*np.eye(N)
 
                 FI_all_kappa[i] = compute_FI(sigma_x, kappa, kappa, T_all, stim_space, mu, gamma, covariances_all)
                 
@@ -314,44 +333,44 @@ if __name__ == '__main__':
                 kappa_MSE_experimental[i] = MSE_powerlaw_experimental(FI_all_kappa[i], target_experimental_precisions)
                 kappa_powerlaw_params[i] = fit_powerlaw(np.arange(1, 6), FI_all_kappa[i])
 
-            plt.figure()
-            plt.semilogy(kappa_space, kappa_MSE_experimental)
-
-            # Show the best sigma
-            best_kappa_ind = np.argmin(kappa_MSE_experimental)
-
-            print "Best kappa: %.2f. MSE: %.2f" % (kappa_space[best_kappa_ind], kappa_MSE_experimental[best_kappa_ind])
-
-            # Precision curve
-            plt.figure()
-            plt.plot(T_all, FI_all_kappa[best_kappa_ind])
-            plt.plot(T_all, target_experimental_precisions)
-            plt.legend(['Model', 'Experimental'])
-
-            # 2D effect of sigma, not good
             # plt.figure()
-            # plt.imshow(FI_all_kappa.T, norm=plt.matplotlib.colors.LogNorm())
+            # plt.semilogy(kappa_space, kappa_MSE_experimental)
 
-            # 3D effect of kappa, nice enough. Smoother than sigmax
-            f = plt.figure()
-            ax = f.add_subplot(111, projection='3d')
-            X, Y = np.meshgrid(kappa_space, T_all)
-            ax.plot_surface(X, Y, np.log(FI_all_kappa.T))
+            # # Show the best sigma
+            # best_kappa_ind = np.argmin(kappa_MSE_experimental)
 
-            # Effect of kappa on the power law exponent: null.
-            plt.figure()
-            plt.plot(kappa_space, kappa_powerlaw_params[:, 0])
+            # print "Best kappa: %.2f. MSE: %.2f" % (kappa_space[best_kappa_ind], kappa_MSE_experimental[best_kappa_ind])
 
-            # Effect of kappa on the power law y0 value: same as FI[0]
-            plt.figure()
-            plt.semilogy(kappa_space, kappa_powerlaw_params[:, 1])
+            # # Precision curve
+            # plt.figure()
+            # plt.plot(T_all, FI_all_kappa[best_kappa_ind])
+            # plt.plot(T_all, target_experimental_precisions)
+            # plt.legend(['Model', 'Experimental'])
+
+            # # 2D effect of sigma, not good
+            # # plt.figure()
+            # # plt.imshow(FI_all_kappa.T, norm=plt.matplotlib.colors.LogNorm())
+
+            # # 3D effect of kappa, nice enough. Smoother than sigmax
+            # f = plt.figure()
+            # ax = f.add_subplot(111, projection='3d')
+            # X, Y = np.meshgrid(kappa_space, T_all)
+            # ax.plot_surface(X, Y, np.log(FI_all_kappa.T))
+
+            # # Effect of kappa on the power law exponent: null.
+            # plt.figure()
+            # plt.plot(kappa_space, kappa_powerlaw_params[:, 0])
+
+            # # Effect of kappa on the power law y0 value: same as FI[0]
+            # plt.figure()
+            # plt.semilogy(kappa_space, kappa_powerlaw_params[:, 1])
             
             # Fisher information for single object, as function of kappa
             plt.figure()
-            plt.plot(kappa_space**2., FI_all_kappa[:, 0])
+            plt.plot(kappa_space, FI_all_kappa[:, 0])
             plt.xlabel('kappa')
-            plt.xlim([0, 10])
-            plt.ylim([0, 10000])
+            # plt.xlim([0, 10])
+            # plt.ylim([0, 10000])
             plt.ylabel('FI for single item')
 
 
@@ -407,9 +426,65 @@ if __name__ == '__main__':
             plt.plot(np.arange(1, 6), FI_all_kappasigma[best_kappasigma_ind])
             plt.legend(['Experimental', 'FI'])
 
+    if True:
+        # Fisher info, new try, 13.08.12
+        kappa = 1.0
+        kappa2 = 1.0
+        sigma = 1.0
+
+        N_sqrt = 1.
+        N = int(N_sqrt**2.)
+        coverage_1D = np.linspace( -np.pi + np.pi/N_sqrt, np.pi + np.pi/N_sqrt, N_sqrt, endpoint=False)
+        means = np.array(cross(2*[coverage_1D.tolist()]))
+
+        precision_space = 200.
+        phi_space = np.linspace(-np.pi, np.pi, precision_space)
+        psi_space = np.linspace(-np.pi, np.pi, precision_space)
+
+        # Slow way
+        # fisher_info = np.zeros((phi_space.size, psi_space.size))
+        # for i, phi in enumerate(phi_space):
+        #     for j, psi in enumerate(psi_space):
+        #         fisher_info[i, j] = (kappa1**2.)/(sigma**2.*16.*np.pi**2.*scsp.i0(kappa1)**2.0*scsp.i0(kappa1)**2.0)*np.sum(np.sin(phi - means[:, 0])**2.*np.exp(2.*kappa1*np.cos(phi - means[:, 0]) + 2.*kappa1*np.cos(psi - means[:, 1])))
+
+        # Array way
+        stimulus_space = np.array(cross(phi_space, psi_space))
+        fisher_info_arr = (kappa**2.)/(sigma**2.*16.*np.pi**4.*scsp.i0(kappa)**2.0*scsp.i0(kappa)**2.0)*np.sin(stimulus_space[:, 0] - means[:, 0][:, np.newaxis])**2.*np.exp(2.*kappa*np.cos(stimulus_space[:, 0] - means[:, 0][:, np.newaxis]) + 2.*kappa*np.cos(stimulus_space[:, 1] - means[:, 1][:, np.newaxis]))
+        mean_FI_total = fisher_info_arr.mean(axis=-1).sum()
+        # fisher_info_arr.shape = (phi_space.size, psi_space.size)
+
+        # Multiple kappas
+        kappa_space = np.linspace(0.05, 15.0, 100.0)
+        fisher_info_kappa = np.zeros((kappa_space.size, precision_space*precision_space))
+        for i, kappa in enumerate(kappa_space):
+            # IF_{psi psi}
+            fisher_info_kappa[i] = ((kappa**2.)/(sigma**2.*16.*np.pi**4.*scsp.i0(kappa)**2.0*scsp.i0(kappa)**2.0)*np.sin(stimulus_space[:, 0] - means[:, 0][:, np.newaxis])**2.*np.exp(2.*kappa*np.cos(stimulus_space[:, 0] - means[:, 0][:, np.newaxis]) + 2.*kappa*np.cos(stimulus_space[:, 1] - means[:, 1][:, np.newaxis]))).mean(axis=-1).sum(axis=-1)
+
+            # IF_{psi phi} = 0
+            # fisher_info_kappa[i] = ((kappa**2.)/(sigma**2.*16.*np.pi**4.*scsp.i0(kappa)**2.0*scsp.i0(kappa)**2.0)*np.sin(stimulus_space[:, 0] - means[:, 0][:, np.newaxis])*np.sin(stimulus_space[:, 1] - means[:, 1][:, np.newaxis])*np.exp(2.*kappa*np.cos(stimulus_space[:, 0] - means[:, 0][:, np.newaxis]) + 2.*kappa*np.cos(stimulus_space[:, 1] - means[:, 1][:, np.newaxis]))).mean(axis=-1).sum(axis=-1)
+
+        # Put the squareroot kappa in, as it is done in RandomFactorialNetwork
+        fisher_info_kappasqrt = np.zeros((kappa_space.size, N, precision_space*precision_space))
+        for i, kappa in enumerate(kappa_space):
+            kappa = kappa**0.5
+            fisher_info_kappasqrt[i] = (kappa**2.)/(sigma**2.*16.*np.pi**2.*scsp.i0(kappa)**2.0*scsp.i0(kappa)**2.0)*np.sin(stimulus_space[:, 0] - means[:, 0][:, np.newaxis])**2.*np.exp(2.*kappa*np.cos(stimulus_space[:, 0] - means[:, 0][:, np.newaxis]) + 2.*kappa*np.cos(stimulus_space[:, 1] - means[:, 1][:, np.newaxis]))
+
+        # Plot
+        plt.figure()
+        plt.plot(kappa_space, fisher_info_kappa.mean(axis=-1))
+        
+        ### For N>1, just multiply FI_1 by N, as everything is translational invariant.
+        N_other = 17.**2.
+        plt.figure()
+        plt.plot(kappa_space, fisher_info_kappa.mean(axis=-1)*N_other)
+        
+        plt.figure()
+        plt.plot(kappa_space, fisher_info_kappasqrt.mean(axis=-1))
 
 
+        plt.show()
 
+        
 
 
 
