Fixed bug for FI estimated from precision in 2D population
- Fixed: Now correctly estimate FI using the precision on the posterior directly. Do not use the wrong x axis and dx….

fisherinformation.py
@@ -755,7 +755,7 @@ if __name__ == '__main__':
         N_sqrt = int(np.sqrt(N))
         kappa1 = 1.0
         kappa2 = 1.0
-        sigma = 0.1
+        sigma = 0.2
         amplitude = 1.0
 
         ## Fisher info
@@ -841,7 +841,7 @@ if __name__ == '__main__':
 
             return lik
 
-        num_points = 200
+        num_points = 500
             
         angles_1D = np.linspace(0., 2.*np.pi, num_points, endpoint=False)
         all_angles = np.array(cross(angles_1D, angles_1D))
@@ -849,8 +849,8 @@ if __name__ == '__main__':
         angles_clamped_fi = np.linspace(0., 2.*np.pi, 1000, endpoint=False)
 
 
-        # kappa_space = np.linspace(0.001, 20., 10)
-        kappa_space = np.linspace(20., 20., 1)
+        # kappa_space = np.linspace(0.001, 30., 20)
+        kappa_space = np.linspace(30., 30., 1)
 
         effects_kappa_mean = []
         effects_kappa_std = []
@@ -861,7 +861,7 @@ if __name__ == '__main__':
             kappa2 = kappa1
 
             ## Generate dataset
-            M = 20
+            M = 100
             # stimuli_used = np.random.rand(M, 2)*np.pi*2.
             stimuli_used = np.random.rand(M, 2)*np.pi/2. + np.pi*3/4.
             stimuli_used[0] = np.array([np.pi-0.4, np.pi+0.4])
@@ -902,22 +902,22 @@ if __name__ == '__main__':
                 posterior_clamped /= np.sum(posterior_clamped*dx_clamped)
 
                 fisher_info_curve_clamped[m] = np.trapz(-np.gradient(np.gradient(log_posterior_clamped))*posterior_clamped/dx_clamped**2., angles_clamped_fi)
-
                 
+
                 #fisher_info_prec[m] = 1./fit_gaussian(all_angles, posterior, should_plot=False, return_fitted=False)[1]**2.
-                # fisher_info_prec[m] = 1./(-2.*np.log(np.abs(np.trapz(posterior*np.exp(1j*all_angles), all_angles))))
+                fisher_info_prec[m] = 1./(-2.*np.log(np.abs(np.trapz(posterior_clamped*np.exp(1j*angles_clamped_fi), angles_clamped_fi))))
 
             fisher_info_curve_mean = np.mean(fisher_info_curve)
             fisher_info_curve_std = np.std(fisher_info_curve)
-            # fisher_info_prec_mean = np.mean(fisher_info_prec)
-            # fisher_info_prec_std = np.std(fisher_info_prec)
+            fisher_info_prec_mean = np.mean(fisher_info_prec)
+            fisher_info_prec_std = np.std(fisher_info_prec)
             fisher_info_curve_clamped_mean = np.mean(fisher_info_curve_clamped)
             fisher_info_curve_clamped_std = np.std(fisher_info_curve_clamped)
 
-            print "FI curve: %.3f, FI clamped: %.3f, Theo: %.3f, Theo large N: %.3f" % (fisher_info_curve_mean, fisher_info_curve_clamped_mean, fisher_info_2D_N(pref_angles=pref_angles, N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma), fisher_info_2D_Ninf(N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma))
+            print "FI precision: %.3f, FI clamped: %.3f, Theo: %.3f, Theo large N: %.3f" % (fisher_info_prec_mean, fisher_info_curve_clamped_mean, fisher_info_2D_N(pref_angles=pref_angles, N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma), fisher_info_2D_Ninf(N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma))
 
-            effects_kappa_mean.append((fisher_info_curve_mean, fisher_info_curve_clamped_mean, fisher_info_2D_N(pref_angles=pref_angles, N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma), fisher_info_2D_Ninf(N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma)))
-            effects_kappa_std.append((fisher_info_curve_std, fisher_info_curve_clamped_std, 0.0, 0.0))
+            effects_kappa_mean.append((fisher_info_prec_mean, fisher_info_curve_clamped_mean, fisher_info_2D_N(pref_angles=pref_angles, N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma), fisher_info_2D_Ninf(N=N, kappa1=kappa1, kappa2=kappa2, sigma=sigma)))
+            effects_kappa_std.append((fisher_info_prec_std, fisher_info_curve_clamped_std, 0.0, 0.0))
 
 
         effects_kappa_mean = np.array(effects_kappa_mean)
@@ -925,7 +925,7 @@ if __name__ == '__main__':
 
         plot_multiple_mean_std_area(kappa_space, effects_kappa_mean.T, effects_kappa_std.T)
 
-        plt.legend(['Curve', 'Clamped', 'Theo', 'Theo large N'])
+        plt.legend(['Precision from 1D Clamped', '1D Clamped curvature', '2D Theo', '2D Theo large N'])
 
 
 