Refactored likelihood computation, fixed precision estimation
- Fixed: Single function call for the likelihood estimation, avoids stupid bugs…
- Fixed: precision estimation in a few methods. (#10)

gibbs_sampler_continuous_fullcollapsed_randomfactorialnetwork.py
@@ -7,7 +7,6 @@ Created by Loic Matthey on 2011-06-1.
 Copyright (c) 2011 Gatsby Unit. All rights reserved.
 """
 
-import pylab as plt
 import numpy as np
 import scipy.special as scsp
 # from scipy.stats import vonmises as vm
@@ -29,6 +28,22 @@ from slicesampler import *
 from utils import *
 from dataio import *
 
+def loglike_theta_fct_vect(thetas, (datapoint, rn, theta_mu, theta_kappa, ATtcB, sampled_feature_index, mean_fixed_contrib, covariance_fixed_contrib)):
+    '''
+        Compute the loglikelihood of: theta_r | n_tc theta_r' tc
+    '''
+    
+    like_mean = datapoint - mean_fixed_contrib - \
+                np.dot(ATtcB, rn.get_network_response(thetas))
+            
+    # Using inverse covariance as param
+    # return theta_kappa*np.cos(thetas[sampled_feature_index] - theta_mu) - 0.5*np.dot(like_mean, np.dot(covariance_fixed_contrib, like_mean))
+    return -0.5*np.dot(like_mean, np.dot(covariance_fixed_contrib, like_mean))
+    # return -1./(2*0.2**2)*np.sum(like_mean**2.)
+
+    # TODO Found the error for the mismatch between samples and posterior here... Using two functions is retarded, should refactore it.
+    # Something funny though: this could be a nice way to get the mismatch between the sigma I approximation to the full covariance matrix.
+
 
 def loglike_theta_fct_single(new_theta, (thetas, datapoint, rn, theta_mu, theta_kappa, ATtcB, sampled_feature_index, mean_fixed_contrib, covariance_fixed_contrib)):
     '''
@@ -43,7 +58,7 @@ def loglike_theta_fct_single(new_theta, (thetas, datapoint, rn, theta_mu, theta_
     # Using inverse covariance as param
     # return theta_kappa*np.cos(thetas[sampled_feature_index] - theta_mu) - 0.5*np.dot(like_mean, np.dot(covariance_fixed_contrib, like_mean))
     return -0.5*np.dot(like_mean, np.dot(covariance_fixed_contrib, like_mean))
-    # return -1./(2*0.1**2)*np.sum(like_mean**2.)
+    # return -1./(2*0.2**2)*np.sum(like_mean**2.)
 
 
 def loglike_theta_fct_single_min(x, thetas, datapoint, rn, theta_mu, theta_kappa, ATtcB, sampled_feature_index, mean_fixed_contrib, covariance_fixed_contrib):
@@ -361,7 +376,7 @@ class Sampler:
         params = (self.theta[n], self.NT[n], self.random_network, self.theta_gamma, self.theta_kappa, self.ATtcB[self.tc[n]], sampled_feature_index, self.mean_fixed_contrib[self.tc[n]], self.covariance_fixed_contrib)
 
         # Sample the new theta
-        samples, llh = self.slicesampler.sample_1D_circular(num_samples, np.random.rand()*2.*np.pi-np.pi, loglike_theta_fct_single, burn=burn_samples, widths=np.pi/3., loglike_fct_params=params, debug=False, step_out=True)
+        samples, llh = self.slicesampler.sample_1D_circular(num_samples, np.random.rand()*2.*np.pi-np.pi, loglike_theta_fct_single, burn=burn_samples, widths=np.pi/5., loglike_fct_params=params, debug=False, step_out=True)
         # samples, llh = self.slicesampler.sample_1D_circular(num_samples, np.random.rand()*2.*np.pi-np.pi, loglike_theta_fct_single, burn=burn_samples, widths=0.01, loglike_fct_params=params, debug=False, step_out=True)
         
         # samples, llh = self.slicesampler.sample_1D_circular(1, self.theta[n, sampled_feature_index], loglike_theta_fct, burn=100, widths=np.pi/3., thinning=2, loglike_fct_params=params, debug=False, step_out=True)
@@ -644,6 +659,9 @@ class Sampler:
             Plot the result
         '''
 
+        # Pack the parameters for the likelihood function
+        params = (self.NT[n], self.random_network, self.theta_gamma, self.theta_kappa, self.ATtcB[t], sampled_feature_index, self.mean_fixed_contrib[t], self.covariance_fixed_contrib)
+        
 
         all_angles = np.linspace(-np.pi, np.pi, num_points, endpoint=False)
         llh_2angles = np.zeros((num_points, num_points))
@@ -652,12 +670,7 @@ class Sampler:
         for i in np.arange(num_points):
             print "%d%%" % (i/float(num_points)*100)
             for j in np.arange(num_points):
-
-                # Pack the parameters for the likelihood function
-                params = (np.array([all_angles[i], all_angles[j]]), self.NT[n], self.random_network, self.theta_gamma, self.theta_kappa, self.ATtcB[t], sampled_feature_index, self.mean_fixed_contrib[t], self.covariance_fixed_contrib)
-        
-                # llh_2angles[i, j] = loglike_theta_fct_vect(np.array([all_angles[i], all_angles[j]]), params)
-                llh_2angles[i, j] = loglike_theta_fct_single(all_angles[i], params)
+                llh_2angles[i, j] = loglike_theta_fct_vect(np.array([all_angles[i], all_angles[j]]), params)
         
         if should_exponentiate:
             llh_2angles = np.exp(llh_2angles)
@@ -707,23 +720,19 @@ class Sampler:
             Plot the log-likelihood function, over the space of the sampled theta, keeping the other thetas fixed to their correct cued value.
         '''
 
-        num_points = int(num_points)
-
         all_angles = np.linspace(-np.pi, np.pi, num_points, endpoint=False)
         llh_2angles = np.zeros((self.T, num_points))
         
         # Compute the array
         for t in np.arange(self.T):
 
+            # Pack the parameters for the likelihood function
+            params = (self.NT[n], self.random_network, self.theta_gamma, self.theta_kappa, self.ATtcB[t], sampled_feature_index, self.mean_fixed_contrib[t], self.covariance_fixed_contrib)
+            
             # Compute the loglikelihood for all possible first feature
             for i in np.arange(num_points):
-
-                # Pack the parameters for the likelihood function
-                params = (np.array([all_angles[i], self.data_gen.stimuli_correct[n, t, 1]]), self.NT[n], self.random_network, self.theta_gamma, self.theta_kappa, self.ATtcB[t], sampled_feature_index, self.mean_fixed_contrib[t], self.covariance_fixed_contrib)
-            
                 # Give the correct cued second feature
-                llh_2angles[t, i] = loglike_theta_fct_single(all_angles[i], params)
-                # llh_2angles[t, i] = loglike_theta_fct_vect(np.array([all_angles[i], self.data_gen.stimuli_correct[n, t, 1]]), params)
+                llh_2angles[t, i] = loglike_theta_fct_vect(np.array([all_angles[i], self.data_gen.stimuli_correct[n, t, 1]]), params)
         
         llh_2angles = llh_2angles.T
         