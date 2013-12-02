04754b3218ec765b71b395d3fabb381688efa4b0
Loic Matthey <loic.matthey@vtxnet.ch>
2013-12-02 13:36:06 +0000

    EM Fitting multiple nontargets, same kappa
    - Added: EM Fitting procedure for model with multiple mixture nontargets, but unique kappa between all angles.
    - Added: Modified Bootstrap estimator to use this new model.
    - Added: Generators for the new bootstrap code.

--- launchers_bootstrap.py
+++ launchers_bootstrap.py
@@ -14,6 +14,7 @@ import utils
 import dataio as DataIO
 import em_circularmixture
 import em_circularmixture_allitems
+import em_circularmixture_allitems_uniquekappa
 
 import launchers
 
@@ -44,16 +45,21 @@ def launcher_do_nontarget_bootstrap(args):
 
     # Load the data
     model_outputs = utils.load_npy( '/nfs/home2/lmatthey/Dropbox/UCL/1-phd/Work/Visual_working_memory/code/git-bayesian-visual-working-memory/Experiments/error_distribution/error_distribution_conj_M100T6repetitions5_121113_outputs/global_plots_errors_distribution-plots_errors_distribution-cc1a49b0-f5f0-4e82-9f0f-5a16a2bfd4e8.npy')
-    # model_outputs = utils.load_npy( '/Users/loicmatthey/Dropbox/UCL/1-phd/Work/Visual_working_memory/code/git-bayesian-visual-working-memory/Experiments/error_distribution/error_distribution_conj_M100T6repetitions5_121113_outputs/global_plots_errors_distribution-plots_errors_distribution-cc1a49b0-f5f0-4e82-9f0f-5a16a2bfd4e8.npy')
+    # model_outputs = utils.load_npy( '/Users/loicmatthey/Dropbox/UCL/1-phd/Work/Visual_working_memory/code/git-bayesian-visual-working-memory/Experiments/bootstrap_nontargets/global_plots_errors_distribution-plots_errors_distribution-cc1a49b0-f5f0-4e82-9f0f-5a16a2bfd4e8.npy')
+
     data_responses_all = model_outputs['result_responses_all'][..., 0]
     data_target_all = model_outputs['result_target_all'][..., 0]
     data_nontargets_all = model_outputs['result_nontargets_all'][..., 0]
     T_space = model_outputs['T_space']
     sigmax_space = model_outputs['sigmax_space']
 
+    K = data_nontargets_all.shape[-1]
+
     # Result arrays
     result_bootstrap_samples_allitems = np.nan*np.ones((sigmax_space.size, T_space.size, all_parameters['num_repetitions']))
     result_bootstrap_samples = np.nan*np.ones((sigmax_space.size, T_space.size, all_parameters['num_repetitions']))
+    result_bootstrap_samples_allitems_uniquekappa_sumnontarget = np.nan*np.ones((sigmax_space.size, T_space.size, all_parameters['num_repetitions']))
+    result_bootstrap_samples_allitems_uniquekappa_allnontarget = np.nan*np.ones((sigmax_space.size, T_space.size, K*all_parameters['num_repetitions']))
 
     search_progress = progress.Progress(sigmax_space.size*(T_space.size-1))
 
@@ -70,12 +76,18 @@ def launcher_do_nontarget_bootstrap(args):
             ### WORK WORK WORK work? ###
 
             # Get some bootstrap samples
-            bootstrap_allitems_nontargets_allitems = em_circularmixture_allitems.bootstrap_nontarget_stat(
+            bootstrap_allitems_nontargets_allitems_uniquekappa = em_circularmixture_allitems_uniquekappa.bootstrap_nontarget_stat(
                     data_responses_all[sigmax_i, T_i],
                     data_target_all[sigmax_i, T_i],
                     data_nontargets_all[sigmax_i, T_i, :, :T_i],
                     nb_bootstrap_samples=all_parameters['num_repetitions'],
                     resample_targets=False)
+            # bootstrap_allitems_nontargets_allitems = em_circularmixture_allitems.bootstrap_nontarget_stat(
+            #         data_responses_all[sigmax_i, T_i],
+            #         data_target_all[sigmax_i, T_i],
+            #         data_nontargets_all[sigmax_i, T_i, :, :T_i],
+            #         nb_bootstrap_samples=all_parameters['num_repetitions'],
+            #         resample_targets=False)
             bootstrap_allitems_nontargets = em_circularmixture.bootstrap_nontarget_stat(
                     data_responses_all[sigmax_i, T_i],
                     data_target_all[sigmax_i, T_i],
@@ -84,12 +96,16 @@ def launcher_do_nontarget_bootstrap(args):
                     resample_targets=False)
 
             # Collect and store responses
-            result_bootstrap_samples_allitems[sigmax_i, T_i] = bootstrap_allitems_nontargets_allitems['nontarget_bootstrap_samples']
+            # result_bootstrap_samples_allitems[sigmax_i, T_i] = bootstrap_allitems_nontargets_allitems['nontarget_bootstrap_samples']
             result_bootstrap_samples[sigmax_i, T_i] = bootstrap_allitems_nontargets['nontarget_bootstrap_samples']
+
+            result_bootstrap_samples_allitems_uniquekappa_sumnontarget[sigmax_i, T_i] = bootstrap_allitems_nontargets_allitems_uniquekappa['nontarget_bootstrap_samples']
+            result_bootstrap_samples_allitems_uniquekappa_allnontarget[sigmax_i, T_i, :all_parameters['num_repetitions']*T_i] = bootstrap_allitems_nontargets_allitems_uniquekappa['allnontarget_bootstrap_samples']
 
             print result_bootstrap_samples_allitems[sigmax_i, T_i]
             print result_bootstrap_samples[sigmax_i, T_i]
-
+            print result_bootstrap_samples_allitems_uniquekappa_sumnontarget[sigmax_i, T_i]
+            print result_bootstrap_samples_allitems_uniquekappa_allnontarget[sigmax_i, T_i]
 
             ### /Work ###
 
