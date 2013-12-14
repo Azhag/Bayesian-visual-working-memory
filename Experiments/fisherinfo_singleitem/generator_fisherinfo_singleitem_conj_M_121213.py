c625949c4262a367b61670592185d8041bf05825
Loic Matthey <loic.matthey@vtxnet.ch>
2013-12-14 15:37:46 +0000

    Generator/Reloader for Fisher Info comparison with different M
    - Add: Was getting bad fits for Fisher info with fixed M=400, so change it on the fly and rerun them. Works better now, for M=100.

--- Experiments/fisherinfo_singleitem/generator_fisherinfo_singleitem_conj_M_121213.py
+++ Experiments/fisherinfo_singleitem/generator_fisherinfo_singleitem_conj_M_121213.py
@@ -14,36 +14,37 @@ parameters_entryscript = dict(action_to_do='launcher_do_generate_submit_pbs_from
 submit_jobs = True
 parameter_generation = 'grid'
 
+M = 100
 num_repetitions = 1
 num_workers = 500
 
 submit_cmd = 'qsub'
 # submit_cmd = 'sbatch'
 
-run_label = 'fisherinfo_singleitem_conj_correctlargen_repetitions{num_repetitions}mult_121213'
+run_label = 'fisherinfo_singleitem_conj_correctlargen_M{M}repetitions{num_repetitions}mult_131213'
 
 pbs_submission_infos = dict(description='Runs multiple metric to get estimate of fisher information for a single item, using a conjunctive code here. Should then put everything back nicely. Corrected larg N FI now.',
                             command='python /nfs/home2/lmatthey/Documents/work/Visual_working_memory/code/git-bayesian-visual-working-memory/experimentlauncher.py',
                             other_options=dict(action_to_do='launcher_do_fisher_information_param_search_pbs',
                                                code_type='conj',
                                                output_directory='.',
-                                               M=100,
-                                               sigmax=0.15,
+                                               M=M,
+                                               sigmax=0.1,
                                                N=500,
                                                T=1,
                                                sigmay=0.0001,
                                                inference_method='sample',
-                                               num_samples=200,
+                                               num_samples=100,
                                                selection_num_samples=1,
                                                selection_method='last',
                                                slice_width=0.07,
-                                               burn_samples=300,
+                                               burn_samples=500,
                                                num_repetitions=num_repetitions,
                                                enforce_min_distance=0.17,
                                                specific_stimuli_random_centers=None,
-                                               stimuli_generation='random',
+                                               stimuli_generation='constant',
                                                stimuli_generation_recall='random',
-                                               autoset_parameters=None,
+                                               rc_scale=4.0,
                                                label=run_label,
                                                experiment_data_dir='/nfs/home2/lmatthey/Dropbox/UCL/1-phd/Work/Visual_working_memory/experimental_data',
                                                ),
