6139f6d26b7c88647485111a168d3cb0fb7259b9
Loic Matthey <loic.matthey@vtxnet.ch>
2014-02-20 17:13:21 +0000

    Generators for FitExperiment

--- Experiments/cmaes_fit_experiment/generator_cmaes_distfitexpbic_mixed_Mratiosigmax_rangebis_allT_200214.py
+++ Experiments/cmaes_fit_experiment/generator_cmaes_distfitexpbic_mixed_Mratiosigmax_rangebis_allT_200214.py
@@ -22,8 +22,7 @@ num_repetitions = 5
 M  = 200
 T = 6
 
-# run_label = 'memory_curve_conj_Msigmax_autoset_correctsampling_repetitions{num_repetitions}_211013'
-run_label = 'cmaes_distfitexpbic_mixed_Mratiosigmax_allT_repetitions{num_repetitions}_170214'
+run_label = 'cmaes_distfitexpbic_mixed_Mratiosigmax_rangebis_allT_repetitions{num_repetitions}_200214'
 simul_out_dir = os.path.join(os.getcwd(), run_label.format(**locals()))
 
 parameter_generation = 'cma-es'
@@ -36,10 +35,10 @@ cma_use_bounds = True
 sleeping_period = dict(min=10, max=30)
 
 # result_callback_function to track best parameter
-best_parameters_seen = dict(result=None, job_name='', parameters=None)
+best_parameters_seen = dict(result=np.nan, job_name='', parameters=None)
 def best_parameters_callback(job, parameters=None):
 
-  if (parameters['result'] is None) or (parameters['result'] is np.nan) or (job.get_result() <= parameters['result'] and not np.isnan(job.get_result())):
+  if not np.any(np.isnan(job.get_result())) and (np.any(np.isnan(parameters['result'])) or (job.get_result() <= parameters['result'])):
       # New best parameter!
       parameters['result'] = job.get_result()
       parameters['job_name'] = job.job_name
@@ -65,13 +64,14 @@ def cma_iter_plot_scatter3d_candidates(all_variables, parameters=None):
 
     candidates_arr = np.array(candidates)
     fitness_arr = np.array(fitness)
+    parameter_names_sorted = all_variables['parameter_names_sorted']
 
     # Do a plot
-    parameters['ax'] = utils.scatter3d(candidates_arr[:, 0], candidates_arr[:, 1], candidates_arr[:, 2], c=np.log(fitness_arr), xlabel=all_variables['parameter_names_sorted'][0], ylabel=all_variables['parameter_names_sorted'][1], zlabel=all_variables['parameter_names_sorted'][2], ax_handle=parameters['ax'])
+    parameters['ax'] = utils.scatter3d(candidates_arr[:, 0], candidates_arr[:, 1], candidates_arr[:, 2], c=np.log(fitness_arr), xlabel=parameter_names_sorted[0], ylabel=parameter_names_sorted[1], zlabel=parameter_names_sorted[2], ax_handle=parameters['ax'])
 
 
     if parameters['dataio'] is not None:
-        parameters['dataio'].save_variables_default(locals(), ['candidates', 'fitness'])
+        parameters['dataio'].save_variables_default(locals(), ['candidates', 'fitness', 'parameter_names_sorted'])
         parameters['dataio'].save_current_figure('cmaes_optim_landscape_scatter3d_{label}_{unique_id}.pdf')
         parameters['dataio'].make_link_output_to_dropbox(dropbox_current_experiment_folder='cmaes_fit_experiment')
 
@@ -110,12 +110,12 @@ pbs_submission_infos = dict(description='Fit experiments (bays09, gorgo11, dualr
                             memory='2gb',
                             simul_out_dir=simul_out_dir,
                             pbs_submit_cmd=submit_cmd,
-                            submit_label='cmaes_3d_bic_all')
+                            submit_label='cmaes_3d_bic_bis')
 
 
-sigmax_range      =   dict(low=0.01, high=3.0, dtype=float)
+sigmax_range      =   dict(low=0.00001, high=1.5, dtype=float)
 ratioconj_range   =   dict(low=0.0, high=1.0, dtype=float)
-M_range           =   dict(low=10, high=800, dtype=int)
+M_range           =   dict(low=2, high=500, dtype=int)
 
 dict_parameters_range =   dict(ratio_conj=ratioconj_range, sigmax=sigmax_range, M=M_range)
 
--- Experiments/cmaes_fit_experiment/generator_cmaes_distfitexpbic_mixed_Mratiosigmaxsigmay_allT_200214.py
+++ Experiments/cmaes_fit_experiment/generator_cmaes_distfitexpbic_mixed_Mratiosigmaxsigmay_allT_200214.py
@@ -22,8 +22,7 @@ num_repetitions = 5
 M  = 200
 T = 6
 
-# run_label = 'memory_curve_conj_Msigmax_autoset_correctsampling_repetitions{num_repetitions}_211013'
-run_label = 'cmaes_distfitexpbic_mixed_Mratiosigmax_allT_repetitions{num_repetitions}_170214'
+run_label = 'cmaes_distfitexpbic_mixed_Mratiosigmaxsigmay_allT_repetitions{num_repetitions}_200214'
 simul_out_dir = os.path.join(os.getcwd(), run_label.format(**locals()))
 
 parameter_generation = 'cma-es'
@@ -36,10 +35,10 @@ cma_use_bounds = True
 sleeping_period = dict(min=10, max=30)
 
 # result_callback_function to track best parameter
-best_parameters_seen = dict(result=None, job_name='', parameters=None)
+best_parameters_seen = dict(result=np.nan, job_name='', parameters=None)
 def best_parameters_callback(job, parameters=None):
 
-  if (parameters['result'] is None) or (parameters['result'] is np.nan) or (job.get_result() <= parameters['result'] and not np.isnan(job.get_result())):
+  if not np.any(np.isnan(job.get_result())) and (np.any(np.isnan(parameters['result'])) or (job.get_result() <= parameters['result'])):
       # New best parameter!
       parameters['result'] = job.get_result()
       parameters['job_name'] = job.job_name
@@ -65,19 +64,34 @@ def cma_iter_plot_scatter3d_candidates(all_variables, parameters=None):
 
     candidates_arr = np.array(candidates)
     fitness_arr = np.array(fitness)
+    parameter_names_sorted = all_variables['parameter_names_sorted']
 
-    # Do a plot
-    parameters['ax'] = utils.scatter3d(candidates_arr[:, 0], candidates_arr[:, 1], candidates_arr[:, 2], c=np.log(fitness_arr), xlabel=all_variables['parameter_names_sorted'][0], ylabel=all_variables['parameter_names_sorted'][1], zlabel=all_variables['parameter_names_sorted'][2], ax_handle=parameters['ax'])
-
-
+    # Save data
     if parameters['dataio'] is not None:
-        parameters['dataio'].save_variables_default(locals(), ['candidates', 'fitness'])
-        parameters['dataio'].save_current_figure('cmaes_optim_landscape_scatter3d_{label}_{unique_id}.pdf')
+        parameters['dataio'].save_variables_default(locals(), ['candidates', 'fitness', 'parameter_names_sorted'])
         parameters['dataio'].make_link_output_to_dropbox(dropbox_current_experiment_folder='cmaes_fit_experiment')
+
+    # Do plots
+    # Param 1 vs 2 vs 3
+    parameters['ax'] = utils.scatter3d(candidates_arr[:, 0], candidates_arr[:, 1], candidates_arr[:, 2], c=np.log(fitness_arr), xlabel=parameter_names_sorted[0], ylabel=parameter_names_sorted[1], zlabel=parameter_names_sorted[2], ax_handle=parameters['ax'])
+    if parameters['dataio'] is not None:
+      parameters['dataio'].save_current_figure('cmaes_optim_landscape_123_scatter3d_{label}_{unique_id}.pdf')
+
+    # Param 1 vs 2 vs 4
+    parameters['ax'] = utils.scatter3d(candidates_arr[:, 0], candidates_arr[:, 1], candidates_arr[:, 3], c=np.log(fitness_arr), xlabel=parameter_names_sorted[0], ylabel=parameter_names_sorted[1], zlabel=parameter_names_sorted[3], ax_handle=parameters['ax'])
+    if parameters['dataio'] is not None:
+      parameters['dataio'].save_current_figure('cmaes_optim_landscape_124_scatter3d_{label}_{unique_id}.pdf')
+
+    # Param 2 vs 3 vs 4
+    parameters['ax'] = utils.scatter3d(candidates_arr[:, 1], candidates_arr[:, 2], candidates_arr[:, 3], c=np.log(fitness_arr), xlabel=parameter_names_sorted[1], ylabel=parameter_names_sorted[2], zlabel=parameter_names_sorted[3], ax_handle=parameters['ax'])
+    if parameters['dataio'] is not None:
+      parameters['dataio'].save_current_figure('cmaes_optim_landscape_234_scatter3d_{label}_{unique_id}.pdf')
+
+
 
 cma_iter_callback_function_infos = dict(function=cma_iter_plot_scatter3d_candidates, parameters=cma_iter_parameters)
 
-pbs_submission_infos = dict(description='Fit experiments (bays09, gorgo11, dualrecall), using distfitexpbic ResultComputation), using the CMA-ES code. Looks at all t<=T here. Changes M, ratio_conj and sigmax. Only looks at BIC score.',
+pbs_submission_infos = dict(description='Fit experiments (bays09, gorgo11, dualrecall), using distfitexpbic ResultComputation), using the CMA-ES code. Looks at all t<=T here. Changes M, ratio_conj, sigmax and sigmay. Only looks at BIC score.',
                             command='python $WORKDIR/experimentlauncher.py',
                             other_options=dict(action_to_do='launcher_do_fitexperiment_allT',
                                                code_type='mixed',
@@ -110,14 +124,15 @@ pbs_submission_infos = dict(description='Fit experiments (bays09, gorgo11, dualr
                             memory='2gb',
                             simul_out_dir=simul_out_dir,
                             pbs_submit_cmd=submit_cmd,
-                            submit_label='cmaes_3d_bic_all')
+                            submit_label='cmaes_4d_bic')
 
 
-sigmax_range      =   dict(low=0.01, high=3.0, dtype=float)
+sigmax_range      =   dict(low=0.00001, high=1.0, dtype=float)
+sigmay_range      =   dict(low=0.00001, high=1.0, dtype=float)
 ratioconj_range   =   dict(low=0.0, high=1.0, dtype=float)
-M_range           =   dict(low=10, high=800, dtype=int)
+M_range           =   dict(low=2, high=500, dtype=int)
 
-dict_parameters_range =   dict(ratio_conj=ratioconj_range, sigmax=sigmax_range, M=M_range)
+dict_parameters_range =   dict(ratio_conj=ratioconj_range, sigmax=sigmax_range, M=M_range, sigmay=sigmay_range)
 
 if __name__ == '__main__':
 
