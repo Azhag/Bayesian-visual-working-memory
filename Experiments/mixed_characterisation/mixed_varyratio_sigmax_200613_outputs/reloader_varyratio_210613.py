9ee4eed1370e161b35691a54d76b93a2a0fec8eb
Loic Matthey <loic.matthey@gatsby.ucl.ac.uk>
2015-06-20 17:55:13 +0100

    Fixing gitignore got one file back...

---
+++ Experiments/mixed_characterisation/mixed_varyratio_sigmax_200613_outputs/reloader_varyratio_210613.py
@@ -0,0 +1,159 @@
+"""
+    ExperimentDescriptor for hierarchicalrandomnetwork parameter sweep.
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+
+from experimentlauncher import *
+from dataio import *
+from smooth import *
+import inspect
+
+
+# # Commit @2042319 +
+
+parameters_entryscript=dict(action_to_do='launcher_do_reload_constrained_parameters', output_directory='.')
+
+
+def plots_3dvolume_hierarchical_M_Mlayerone(data_pbs, generator_module=None):
+    '''
+        Reload 3D volume runs from PBS and plot them
+
+    '''
+    
+    print "Order parameters: ", generator_module.dict_parameters_range.keys() 
+
+    # results_precision_ratio_T_all = data_pbs.dict_arrays['results_precision_ratio_T']['results']
+    results_precision_ratio_T = np.squeeze(nanmean(data_pbs.dict_arrays['results_precision_ratio_T']['results'], axis=-1))
+    
+    ratio_space = data_pbs.loaded_data['parameters_uniques']['ratio_conj']
+    
+    T = results_precision_ratio_T.shape[-1]
+    T_space = np.arange(1, T+1)
+    
+    print ratio_space
+    print T_space
+    print results_precision_ratio_T.shape
+
+    dataio = DataIO(output_folder=generator_module.pbs_submission_infos['simul_out_dir'] + '/outputs/', label='global_' + dataset_infos['save_output_filename'])
+    dataio.make_link_in_directory(inspect.getfile(inspect.currentframe()), source_dir='../../')
+
+    savefigs = True
+    
+    plt.rcParams['font.size'] = 14
+
+    pcolor_2d_data(results_precision_ratio_T, log_scale=True, x=ratio_space, y=T_space, xlabel="$\\frac{M_{conj}}{M_{conj}+M_{feature}}$", ylabel='$T$', ticks_interpolate=10)
+    plt.plot(np.argmax(results_precision_ratio_T, axis=0), np.arange(results_precision_ratio_T.shape[-1]), 'ko', markersize=10)
+
+    if savefigs:
+        dataio.save_current_figure('results_2dlog_{label}_global_{unique_id}.pdf')
+
+    pcolor_2d_data(results_precision_ratio_T/np.max(results_precision_ratio_T, axis=0), x=ratio_space, y=T_space, xlabel="$\\frac{M}{M+M_{layer one}}$", ylabel='$T$', ticks_interpolate=10)
+    plt.plot(np.argmax(results_precision_ratio_T, axis=0), np.arange(results_precision_ratio_T.shape[-1]), 'ko', markersize=10)
+
+    if savefigs:
+        dataio.save_current_figure('results_2dnorm_{label}_global_{unique_id}.pdf')
+
+    plt.figure()
+    moved_results_precision_ratio_T = 1.2*np.arange(results_precision_ratio_T.shape[-1]) + results_precision_ratio_T/np.max(results_precision_ratio_T, axis=0)
+    all_lines = []
+    for i, max_i in enumerate(np.argmax(results_precision_ratio_T, axis=0)):
+        curr_lines = plt.plot(ratio_space, moved_results_precision_ratio_T[:, i], linewidth=2)
+        plt.plot(ratio_space[max_i], moved_results_precision_ratio_T[max_i, i], 'o', markersize=10, color=curr_lines[0].get_color())
+        all_lines.extend(curr_lines)
+
+    plt.plot(np.linspace(0.0, 1.0, 100), np.outer(np.ones(100), 1.2*np.arange(1, results_precision_ratio_T.shape[-1])), 'k:')
+    plt.grid()
+    plt.legend(all_lines, ['%d item' % i + 's'*(i>1) for i in xrange(1, T+1)], loc='best')
+    plt.ylim((0., moved_results_precision_ratio_T.max()*1.05))
+    plt.yticks([])
+    plt.xticks(np.linspace(0, 1.0, 5))
+
+    if savefigs:
+        dataio.save_current_figure('results_1dnorm_{label}_global_{unique_id}.pdf')
+
+    # results_precision_ratio_T_smoothed = np.apply_along_axis(smooth, 0, results_precision_ratio_T, *(10, 'bartlett'))
+    # pcolor_2d_data(results_precision_ratio_T_smoothed/np.max(results_precision_ratio_T_smoothed, axis=0), x=ratio_space, y=T_space, xlabel="$\\frac{M}{M+M_{layer one}}$", ylabel='$T$', ticks_interpolate=10)
+    # plt.plot(np.argmax(results_precision_ratio_T_smoothed, axis=0), np.arange(results_precision_ratio_T_smoothed.shape[-1]), 'ko', markersize=10)
+
+    # if savefigs:
+    #     dataio.save_current_figure('results_2dsmoothnorm_{label}_global_{unique_id}.pdf')
+
+    # plt.figure()
+    # plt.plot(ratio_space, results_precision_ratio_T_smoothed/np.max(results_precision_ratio_T_smoothed, axis=0), linewidth=2)
+    # plt.plot(ratio_space[np.argmax(results_precision_ratio_T_smoothed, axis=0)], np.ones(results_precision_ratio_T_smoothed.shape[-1]), 'ro', markersize=10)
+    # plt.grid()
+    # plt.ylim((0., 1.1))
+    # plt.subplots_adjust(right=0.8)
+    # plt.legend(['%d item' % i + 's'*(i>1) for i in xrange(1, T+1)], loc='center right', bbox_to_anchor=(1.3, 0.5))
+    # plt.xticks(np.linspace(0, 1.0, 5))
+
+    # if savefigs:
+        # dataio.save_current_figure('results_1dsmoothnormsame_{label}_global_{unique_id}.pdf')
+
+    # plt.figure()
+    # moved_results_precision_ratio_T_smoothed = 1.2*np.arange(results_precision_ratio_T_smoothed.shape[-1]) + results_precision_ratio_T_smoothed/np.max(results_precision_ratio_T_smoothed, axis=0)
+    # all_lines = []
+    # for i, max_i in enumerate(np.argmax(results_precision_ratio_T_smoothed, axis=0)):
+    #     curr_lines = plt.plot(ratio_space, moved_results_precision_ratio_T_smoothed[:, i], linewidth=2)
+    #     plt.plot(ratio_space[max_i], moved_results_precision_ratio_T_smoothed[max_i, i], 'o', markersize=10, color=curr_lines[0].get_color())
+    #     all_lines.extend(curr_lines)
+
+    # plt.plot(np.linspace(0.0, 1.0, 100), np.outer(np.ones(100), 1.2*np.arange(1, results_precision_ratio_T_smoothed.shape[-1])), 'k:')
+    # plt.grid()
+    # plt.legend(all_lines, ['%d item' % i + 's'*(i>1) for i in xrange(1, T+1)], loc='best')
+    # plt.ylim((0., moved_results_filtered_smoothed.max()*1.05))
+    # plt.yticks([])
+    # plt.xticks(np.linspace(0, 1.0, 5))
+
+    # if savefigs:
+    #     dataio.save_current_figure('results_1dsmoothnorm_{label}_global_{unique_id}.pdf')
+
+
+    variables_to_save = ['results_precision_constant_M_Mlower', 'M_space', 'M_layer_one_space']
+    dataio.save_variables(variables_to_save, locals())
+    plt.show()
+
+
+
+
+generator_script='generator_varyratio_210613.py'
+
+generator_module = imp.load_source(os.path.splitext(generator_script)[0], generator_script)
+dataset_infos = dict(label='Hierarchical network. Assume we want to allocate a fixed number of neurons between the two layers. Do that by constraining the sum of M and M_layer_one to be some constant. Corrected logic so that whole population is accessible now. Feature lower layer.', 
+                     files="%s/%s-*.npy" % (generator_module.pbs_submission_infos['simul_out_dir'], (generator_module.pbs_submission_infos['other_options']['label']).format(**generator_module.pbs_submission_infos['other_options'])),
+                     launcher_module=generator_module,
+                     loading_type='args',
+                     parameters=['ratio_conj'],
+                     variables_to_load=['results_precision_ratio_T'],
+                     variables_description=['Precision volume'],
+                     post_processing=plots_3dvolume_hierarchical_M_Mlayerone,
+                     save_output_filename='3dvolume_hierarchical_constant_M_Mlayerone_feature'
+                     )
+
+
+
+
+if __name__ == '__main__':
+    
+    this_file = inspect.getfile(inspect.currentframe())
+    print "Running ", this_file
+
+    arguments_dict=dict(parameters_filename=this_file)
+    arguments_dict.update(parameters_entryscript)
+    experiment_launcher = ExperimentLauncher(run=True, arguments_dict=arguments_dict)
+
+    variables_to_reinstantiate = ['data_gen', 'sampler', 'stat_meas', 'random_network', 'args', 'constrained_parameters', 'data_pbs', 'dataio']
+
+    if 'variables_to_save' in experiment_launcher.all_vars:
+        # Also reinstantiate the variables we saved
+        variables_to_reinstantiate.extend(experiment_launcher.all_vars['variables_to_save'])
+
+    for var_reinst in variables_to_reinstantiate:
+        if var_reinst in experiment_launcher.all_vars:
+            vars()[var_reinst] = experiment_launcher.all_vars[var_reinst]
+
