0d563d979a57fb42f3c310d231eb218fb93d25e3
Loic Matthey <lmatthey@google.com>
2015-06-29 15:18:42 +0100

    Added launcher for DualRecall FitMixture
    Bit hackier than others (doesn't check T), but works.

--- launchers_fit_mixturemodels.py
+++ launchers_fit_mixturemodels.py
@@ -311,3 +311,124 @@ def launcher_do_fit_mixturemodels_sequential_alltrecall(args):
     return locals()
 
 
+def launcher_do_fit_mixturemodel_dualrecall(args):
+    '''
+        Run the model for T items, trying to fit
+        the DualRecall dataset, which has two conditions.
+
+        Get:
+        - Precision
+        - EM mixture model fits
+        - Theoretical Fisher Information
+        - EM Mixture model distances
+    '''
+
+    print "Doing a piece of work for launcher_do_fit_mixturemodel_dualrecall"
+
+    all_parameters = utils.argparse_2_dict(args)
+    print all_parameters
+
+    if all_parameters['burn_samples'] + all_parameters['num_samples'] < 200:
+        print "WARNING> you do not have enough samples I think!", all_parameters['burn_samples'] + all_parameters['num_samples']
+
+
+    # Create DataIO
+    #  (complete label with current variable state)
+    dataio = DataIO.DataIO(output_folder=all_parameters['output_directory'], label=all_parameters['label'].format(**all_parameters))
+    save_every = 1
+    run_counter = 0
+
+
+    # Load datasets to compare against
+    data_dualrecall = load_experimental_data.load_data_dualrecall(data_dir=all_parameters['experiment_data_dir'], fit_mixture_model=True)
+    dualrecall_T_space = data_dualrecall['data_to_fit']['n_items']
+
+    dualrecall_experimental_angle_emfits_mean = data_dualrecall['em_fits_angle_nitems_arrays']['mean']
+    dualrecall_experimental_colour_emfits_mean = data_dualrecall['em_fits_colour_nitems_arrays']['mean']
+
+    # Parameters to vary
+    repetitions_axis = -1
+
+    # Result arrays
+    result_all_precisions = np.nan*np.empty((all_parameters['num_repetitions']))
+    result_fi_theo = np.nan*np.empty((all_parameters['num_repetitions']))
+    result_fi_theocov = np.nan*np.empty((all_parameters['num_repetitions']))
+    result_em_fits = np.nan*np.empty((6, all_parameters['num_repetitions']))  # kappa, mixt_target, mixt_nontarget, mixt_random, ll, bic
+    result_dist_dualrecall_angle = np.nan*np.empty((4, all_parameters['num_repetitions']))  # kappa, mixt_target, mixt_nontarget, mixt_random
+    result_dist_dualrecall_angle_emmixt_KL = np.nan*np.empty((all_parameters['num_repetitions']))
+    result_dist_dualrecall_colour = np.nan*np.empty((4, all_parameters['num_repetitions']))  # kappa, mixt_target, mixt_nontarget, mixt_random
+    result_dist_dualrecall_colour_emmixt_KL = np.nan*np.empty((all_parameters['num_repetitions']))
+
+    # If desired, will automatically save all Model responses.
+    if all_parameters['collect_responses']:
+        print "--- Collecting all responses..."
+        result_responses = np.nan*np.ones((all_parameters['N'], all_parameters['num_repetitions']))
+        result_target = np.nan*np.ones((all_parameters['N'], all_parameters['num_repetitions']))
+        result_nontargets = np.nan*np.ones((all_parameters['N'], all_parameters['T'] - 1, all_parameters['num_repetitions']))
+
+    search_progress = progress.Progress(all_parameters['num_repetitions'])
+
+    for repet_i in xrange(all_parameters['num_repetitions']):
+        print "%.2f%%, %s left - %s" % (search_progress.percentage(), search_progress.time_remaining_str(), search_progress.eta_str())
+
+        print "Fit for T=%d, %d/%d" % (all_parameters['T'], repet_i+1, all_parameters['num_repetitions'])
+
+        ## Update parameter
+
+        ### WORK WORK WORK work? ###
+        # Instantiate
+        (_, _, _, sampler) = launchers.init_everything(all_parameters)
+
+        # Sample
+        sampler.run_inference(all_parameters)
+
+        # Compute precision
+        print "get precision..."
+        result_all_precisions[repet_i] = sampler.get_precision()
+
+        # Fit mixture model
+        print "fit mixture model..."
+        curr_params_fit = sampler.fit_mixture_model(use_all_targets=False)
+        # curr_params_fit['mixt_nontargets_sum'] = np.sum(curr_params_fit['mixt_nontargets'])
+        result_em_fits[:, repet_i] = [curr_params_fit[key] for key in ['kappa', 'mixt_target', 'mixt_nontargets_sum', 'mixt_random', 'train_LL', 'bic']]
+
+        # Compute fisher info
+        print "compute fisher info"
+        result_fi_theo[repet_i] = sampler.estimate_fisher_info_theocov(use_theoretical_cov=False)
+        result_fi_theocov[repet_i] = sampler.estimate_fisher_info_theocov(use_theoretical_cov=True)
+
+        # Compute distances to datasets
+        if all_parameters['T'] in dualrecall_T_space:
+            # Angle trials
+            result_dist_dualrecall_angle[:, repet_i] = (dualrecall_experimental_angle_emfits_mean[:, dualrecall_T_space == all_parameters['T']].flatten() - result_em_fits[:4, repet_i])**2.
+            result_dist_dualrecall_angle_emmixt_KL[repet_i] = utils.KL_div(result_em_fits[1:4, repet_i], dualrecall_experimental_angle_emfits_mean[1:, dualrecall_T_space==all_parameters['T']].flatten())
+
+            # Colour trials
+            result_dist_dualrecall_colour[:, repet_i] = (dualrecall_experimental_colour_emfits_mean[:, dualrecall_T_space == all_parameters['T']].flatten() - result_em_fits[:4, repet_i])**2.
+            result_dist_dualrecall_colour_emmixt_KL[repet_i] = utils.KL_div(result_em_fits[1:4, repet_i], dualrecall_experimental_colour_emfits_mean[1:, dualrecall_T_space==all_parameters['T']].flatten())
+
+        # If needed, store responses
+        if all_parameters['collect_responses']:
+            (responses, target, nontarget) = sampler.collect_responses()
+            result_responses[:, repet_i] = responses
+            result_target[:, repet_i] = target
+            result_nontargets[..., repet_i] = nontarget
+
+            print "collected responses"
+
+
+        print "CURRENT RESULTS:\n", result_all_precisions[repet_i], curr_params_fit, result_fi_theo[repet_i], result_fi_theocov[repet_i], np.sum(result_dist_dualrecall_angle[:, repet_i]), np.sum(result_dist_dualrecall_colour[:, repet_i]), "\n"
+        ### /Work ###
+
+        search_progress.increment()
+        if run_counter % save_every == 0 or search_progress.done():
+            dataio.save_variables_default(locals())
+        run_counter += 1
+
+    # Finished
+    dataio.save_variables_default(locals())
+
+    print "All finished"
+    return locals()
+
+
