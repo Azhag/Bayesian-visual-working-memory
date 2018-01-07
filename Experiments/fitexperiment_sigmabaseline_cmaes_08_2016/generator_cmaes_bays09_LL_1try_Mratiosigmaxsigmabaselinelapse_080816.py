41c2604c9b9fdb365aca78b0f31a4cc454a27307
Loic Matthey <loic.matthey@gatsby.ucl.ac.uk>
2018-01-07 14:55:09 +0000

    Metric mismatch, All Simultaneous fits, Sequential WIP

--- Experiments/fitexperiment_sigmabaseline_cmaes_08_2016/generator_cmaes_bays09_LL_1try_Mratiosigmaxsigmabaselinelapse_080816.py
+++ Experiments/fitexperiment_sigmabaseline_cmaes_08_2016/generator_cmaes_bays09_LL_1try_Mratiosigmaxsigmabaselinelapse_080816.py
@@ -14,7 +14,9 @@ import submitpbs
 # Commit > @d0d5ff8f2
 
 # Read from other scripts
-parameters_entryscript = dict(action_to_do='launcher_do_generate_submit_pbs_from_param_files', output_directory='.')
+parameters_entryscript = dict(
+    action_to_do='launcher_do_generate_submit_pbs_from_param_files',
+    output_directory='.')
 submit_jobs = True
 # submit_cmd = 'qsub'
 submit_cmd = 'sbatch'
@@ -24,7 +26,6 @@ resource = ''
 # partition = 'wrkstn'
 # partition = 'test'
 partition = 'intel-ivy'
-
 
 num_repetitions = 5
 experiment_id = 'bays09'
@@ -41,65 +42,71 @@ cma_use_bounds = True
 
 sleeping_period = dict(min=10, max=20)
 
-pbs_submission_infos = dict(description='Fit experiments (bays09), using dist_ll_allt ResultComputation), using the CMA-ES code. Now with sigma_baseline instead of sigma_output. Looks at all t<=T here. Changes M, ratio_conj, sigmax, sigma baseline, lapse rate. Only looks at LL score, all datapoints. Looking for best meaningful parameter bounds now. Using new fixed Covariance matrix for Sampler, should change N=1 case most.',
-                            command='python $WORKDIR/experimentlauncher.py',
-                            other_options=dict(action_to_do='launcher_do_fitexperiment_allmetrics',
-                                               code_type='mixed',
-                                               output_directory='.',
-                                               experiment_id=experiment_id,
-                                               bic_K=5,
-                                               ratio_conj=0.5,
-                                               session_id='cmaes_1try_Mratiosigmaxlapsesigmabase_bays09',
-                                               result_computation='dist_ll_allt',
-                                               M=100,
-                                               sigmax=0.1,
-                                               renormalize_sigma=None,
-                                               N=200,
-                                               T=1,
-                                               sigmay=0.00001,
-                                               sigma_baseline=0.001,
-                                               sigma_output=0.0,
-                                               lapse_rate=0.0,
-                                               inference_method='none',
-                                               num_samples=100,
-                                               selection_num_samples=1,
-                                               selection_method='last',
-                                               slice_width=0.07,
-                                               burn_samples=200,
-                                               num_repetitions=num_repetitions,
-                                               enforce_min_distance=0.17,
-                                               specific_stimuli_random_centers=None,
-                                               stimuli_generation='random',
-                                               stimuli_generation_recall='random',
-                                               autoset_parameters=None,
-                                               collect_responses=None,
-                                               label=run_label,
-                                               experiment_data_dir=os.path.normpath(os.path.join(os.environ['WORKDIR_DROP'], '../../experimental_data')),
-                                               ),
-                            walltime='1:00:00',
-                            memory='2gb',
-                            simul_out_dir=os.path.join(os.getcwd(), run_label.format(**locals())),
-                            pbs_submit_cmd=submit_cmd,
-                            source_dir=os.environ['WORKDIR_DROP'],
-                            submit_label='cmaes_1try_bays',
-                            resource=resource,
-                            partition=partition,
-                            qos='auto')
-
+pbs_submission_infos = dict(
+    description=
+    'Fit experiments (bays09), using dist_ll_allt ResultComputation), using the CMA-ES code. Now with sigma_baseline instead of sigma_output. Looks at all t<=T here. Changes M, ratio_conj, sigmax, sigma baseline, lapse rate. Only looks at LL score, all datapoints. Looking for best meaningful parameter bounds now. Using new fixed Covariance matrix for Sampler, should change N=1 case most.',
+    command='python $WORKDIR/experimentlauncher.py',
+    other_options=dict(
+        action_to_do='launcher_do_fitexperiment_allmetrics',
+        code_type='mixed',
+        output_directory='.',
+        experiment_id=experiment_id,
+        bic_K=5,
+        ratio_conj=0.5,
+        session_id='cmaes_1try_Mratiosigmaxlapsesigmabase_bays09',
+        result_computation='dist_ll_allt',
+        M=100,
+        sigmax=0.1,
+        renormalize_sigma=None,
+        N=200,
+        T=1,
+        sigmay=0.00001,
+        sigma_baseline=0.001,
+        sigma_output=0.0,
+        lapse_rate=0.0,
+        inference_method='none',
+        num_samples=100,
+        selection_num_samples=1,
+        selection_method='last',
+        slice_width=0.07,
+        burn_samples=200,
+        num_repetitions=num_repetitions,
+        enforce_min_distance=0.17,
+        specific_stimuli_random_centers=None,
+        stimuli_generation='random',
+        stimuli_generation_recall='random',
+        autoset_parameters=None,
+        collect_responses=None,
+        label=run_label,
+        experiment_data_dir=os.path.normpath(
+            os.path.join(os.environ['WORKDIR_DROP'],
+                         '../../experimental_data')),
+    ),
+    walltime='1:00:00',
+    memory='2gb',
+    simul_out_dir=os.path.join(os.getcwd(), run_label.format(**locals())),
+    pbs_submit_cmd=submit_cmd,
+    source_dir=os.environ['WORKDIR_DROP'],
+    submit_label='cmaes_1try_bays',
+    resource=resource,
+    partition=partition,
+    qos='auto')
 
 
 ## Define our filtering function
-def filtering_function(new_parameters, dict_parameters_range, function_parameters=None):
+def filtering_function(new_parameters,
+                       dict_parameters_range,
+                       function_parameters=None):
     '''
     Given M and ratio_conj, will adapt them so that M_conj is always correct and integer.
 
     or if should_clamp is False, will not change them
     '''
-    M_conj_prior = int(new_parameters['M']*new_parameters['ratio_conj'])
+    M_conj_prior = int(new_parameters['M'] * new_parameters['ratio_conj'])
     M_conj_true = int(np.floor(M_conj_prior**0.5)**2)
-    M_feat_true = int(np.floor((new_parameters['M']-M_conj_prior)/2.)*2.)
+    M_feat_true = int(np.floor((new_parameters['M'] - M_conj_prior) / 2.) * 2.)
     M_true = M_conj_true + M_feat_true
-    ratio_true = M_conj_true/float(M_true)
+    ratio_true = M_conj_true / float(M_true)
 
     if function_parameters['should_clamp']:
         # Clamp them and return true
@@ -110,35 +117,68 @@ def filtering_function(new_parameters, dict_parameters_range, function_parameter
     else:
         return np.allclose(M_true, new_parameters['M'])
 
+
 filtering_function_parameters = {'should_clamp': True}
 
-
 # ============================================================================
-sigmax_range = dict(sampling_type='uniform', low=0.1, high=0.8, dtype=float)
-sigmabaseline_range = dict(sampling_type='uniform', low=0.01, high=0.8, dtype=float)
-ratioconj_range = dict(sampling_type='uniform', low=0.3, high=1.0, dtype=float)
-lapserate_range = dict(sampling_type='uniform', low=0.0, high=0.15, dtype=float)
-M_range = dict(sampling_type='randint', low=6, high=400, dtype=int)
+sigmax_range = dict(
+    sampling_type='uniform',
+    low=0.1,
+    high=0.8,
+    dtype=float)
+sigmabaseline_range = dict(
+    sampling_type='uniform',
+    low=0.01,
+    high=0.8,
+    dtype=float)
+ratioconj_range = dict(
+    sampling_type='uniform',
+    low=0.3,
+    high=1.0,
+    dtype=float)
+lapserate_range = dict(
+    sampling_type='uniform',
+    low=0.0,
+    high=0.15,
+    dtype=float)
+M_range = dict(
+    sampling_type='randint',
+    low=6,
+    high=400,
+    dtype=int)
 
-
-dict_parameters_range = dict(M=M_range, lapse_rate=lapserate_range, ratio_conj=ratioconj_range, sigmax=sigmax_range, sigma_baseline=sigmabaseline_range)
+dict_parameters_range = dict(
+    M=M_range,
+    lapse_rate=lapserate_range,
+    ratio_conj=ratioconj_range,
+    sigmax=sigmax_range,
+    sigma_baseline=sigmabaseline_range)
 # ============================================================================
-
-
-
 
 # result_callback_function to track best parameter
-best_parameters_seen = dict(result=np.nan, job_name='', parameters=None, submit_best=False, pbs_submission_infos_copy=copy.deepcopy(pbs_submission_infos))
+best_parameters_seen = dict(
+    result=np.nan,
+    job_name='',
+    parameters=None,
+    submit_best=False,
+    pbs_submission_infos_copy=copy.deepcopy(pbs_submission_infos))
+
+
 def best_parameters_callback(job, parameters=None):
 
-    if not np.any(np.isnan(job.get_result())) and (np.any(np.isnan(parameters['result'])) or (job.get_result() <= parameters['result'])):
+    if not np.any(np.isnan(job.get_result())) and (
+            np.any(np.isnan(parameters['result'])) or
+        (job.get_result() <= parameters['result'])):
         # New best parameter!
         parameters['result'] = job.get_result()
         parameters['job_name'] = job.job_name
         parameters['parameters'] = job.experiment_parameters
-        parameters['best_parameters'] = utils.subdict(job.experiment_parameters, dict_parameters_range.keys())
+        parameters['best_parameters'] = utils.subdict(
+            job.experiment_parameters, dict_parameters_range.keys())
 
-        print "\n\n>>>>>> Found new best parameters: \n%s %s %s\n\n" % (parameters['best_parameters'], parameters['result'], parameters['job_name'])
+        print "\n\n>>>>>> Found new best parameters: \n%s %s %s\n\n" % (
+            parameters['best_parameters'], parameters['result'],
+            parameters['job_name'])
         parameters['best_parameters']
 
         np.save('./outputs/best_params', dict(parameters=parameters))
@@ -149,38 +189,52 @@ def best_parameters_callback(job, parameters=None):
             pbs_submission_infos_copy = parameters['pbs_submission_infos_copy']
             try:
                 # Will check the best fitting parameters, and relaunch simulations for them, in order to get new cool plots.
-                curr_params_label = '_'.join(["%s%.2f" % (k.replace('_', ''), v) for k, v in parameters['best_parameters'].iteritems()])
+                curr_params_label = '_'.join([
+                    "%s%.2f" % (k.replace('_', ''), v)
+                    for k, v in parameters['best_parameters'].iteritems()
+                ])
 
                 ## First do Memory curves + EM Fits
-                pbs_submission_infos_copy['other_options'].update(dict(
-                    action_to_do='launcher_do_memory_curve_marginal_fi_withplots_live',
-                    subaction='collect_responses',
-                    inference_method='sample',
-                    N=300,
-                    T=6,
-                    num_samples=300,
-                    output_directory=os.path.join(simul_out_dir, 'outputs'),
-                    selection_method='last',
-                    num_repetitions=10,
-                    burn_samples=200,
-                    stimuli_generation='random',
-                    stimuli_generation_recall='random',
-                    session_id='cmaes_bays09_1try_rerun_080816',
-                    result_computation='filenameoutput',
-                    label='%s_cmaes_bays09_1try_080816' % (curr_params_label)
-                ))
+                pbs_submission_infos_copy['other_options'].update(
+                    dict(
+                        action_to_do=
+                        'launcher_do_memory_curve_marginal_fi_withplots_live',
+                        subaction='collect_responses',
+                        inference_method='sample',
+                        N=300,
+                        T=6,
+                        num_samples=300,
+                        output_directory=os.path.join(simul_out_dir,
+                                                      'outputs'),
+                        selection_method='last',
+                        num_repetitions=10,
+                        burn_samples=200,
+                        stimuli_generation='random',
+                        stimuli_generation_recall='random',
+                        session_id='cmaes_bays09_1try_rerun_080816',
+                        result_computation='filenameoutput',
+                        label='%s_cmaes_bays09_1try_080816' %
+                        (curr_params_label)))
                 pbs_submission_infos_copy['walltime'] = '40:00:00'
                 pbs_submission_infos_copy['submit_label'] = 'bestparam_rerun'
 
-                submit_pbs = submitpbs.SubmitPBS(pbs_submission_infos=pbs_submission_infos_copy, debug=True)
+                submit_pbs = submitpbs.SubmitPBS(
+                    pbs_submission_infos=pbs_submission_infos_copy, debug=True)
 
                 # Extract the parameters to try
-                best_params_resend = [utils.subdict(job.experiment_parameters, dict_parameters_range.keys())]
+                best_params_resend = [
+                    utils.subdict(job.experiment_parameters,
+                                  dict_parameters_range.keys())
+                ]
 
                 # Submit without waiting
                 print "Submitting extra job for Plots, parameters:", best_params_resend
-                submission_parameters_dict = dict(pbs_submission_infos=pbs_submission_infos_copy, submit_jobs=submit_jobs, wait_jobs_completed=False)
-                submit_pbs.submit_minibatch_jobswrapper(best_params_resend, submission_parameters_dict)
+                submission_parameters_dict = dict(
+                    pbs_submission_infos=pbs_submission_infos_copy,
+                    submit_jobs=submit_jobs,
+                    wait_jobs_completed=False)
+                submit_pbs.submit_minibatch_jobswrapper(
+                    best_params_resend, submission_parameters_dict)
 
             except Exception as e:
                 print "Failure while submitting sub-task for best parameter. Continuing anyway."
@@ -188,12 +242,17 @@ def best_parameters_callback(job, parameters=None):
                 print e
 
 
-result_callback_function_infos = dict(function=best_parameters_callback, parameters=best_parameters_seen)
+result_callback_function_infos = dict(
+    function=best_parameters_callback, parameters=best_parameters_seen)
 
 # Callback after each iteration, let's save all candidates/fitness, and do a contour plot
-data_io = dataio.DataIO(label='cmaes_alliter_tracking', output_folder=os.path.join(simul_out_dir, 'outputs'))
+data_io = dataio.DataIO(
+    label='cmaes_alliter_tracking',
+    output_folder=os.path.join(simul_out_dir, 'outputs'))
 
 cma_iter_parameters = dict(ax=None, candidates=[], fitness=[], dataio=data_io)
+
+
 def cma_iter_plot_scatter3d_candidates(all_variables, parameters=None):
     candidates = parameters['candidates']
     fitness = parameters['fitness']
@@ -209,8 +268,11 @@ def cma_iter_plot_scatter3d_candidates(all_variables, parameters=None):
 
     # Save data
     if parameters['dataio'] is not None:
-        parameters['dataio'].save_variables_default(locals(), ['candidates', 'fitness', 'parameter_names_sorted'])
-        parameters['dataio'].make_link_output_to_dropbox(dropbox_current_experiment_folder='fitexperiment_sigmabaseline_cmaes_08_2016')
+        parameters['dataio'].save_variables_default(
+            locals(), ['candidates', 'fitness', 'parameter_names_sorted'])
+        parameters['dataio'].make_link_output_to_dropbox(
+            dropbox_current_experiment_folder=
+            'fitexperiment_sigmabaseline_cmaes_08_2016')
 
     # Do plot
     # if parameters['ax'] is None:
@@ -229,8 +291,9 @@ def cma_iter_plot_scatter3d_candidates(all_variables, parameters=None):
     #     parameters['dataio'].save_current_figure('cmaes_optim_timeevolution_{label}_{unique_id}.pdf')
 
 
-cma_iter_callback_function_infos = dict(function=cma_iter_plot_scatter3d_candidates, parameters=cma_iter_parameters)
-
+cma_iter_callback_function_infos = dict(
+    function=cma_iter_plot_scatter3d_candidates,
+    parameters=cma_iter_parameters)
 
 if __name__ == '__main__':
 
@@ -240,5 +303,5 @@ if __name__ == '__main__':
     arguments_dict = dict(parameters_filename=this_file)
     arguments_dict.update(parameters_entryscript)
 
-    experiment_launcher = experimentlauncher.ExperimentLauncher(run=True, arguments_dict=arguments_dict)
-
+    experiment_launcher = experimentlauncher.ExperimentLauncher(
+        run=True, arguments_dict=arguments_dict)
---
+++ Experiments/thesis_plots/random_sweeps_metrics_mismatch/reloader_metricmismatch_ratiosigmax_grid_020118.py
@@ -0,0 +1,90 @@
+"""
+    ExperimentDescriptor for Fitting experiments in a mixed population code
+"""
+
+import os
+import numpy as np
+
+import matplotlib as mpl
+if 'DISPLAY' in os.environ and mpl.get_backend() == 'pdf':
+    # Most likely Gatsby machine interactively, change backend
+    mpl.use('TkAgg')
+
+import matplotlib.pyplot as plt
+
+from experimentlauncher import ExperimentLauncher
+from dataio import DataIO
+import launchers_memorycurves_marginal_fi
+import plots_experimental_data
+
+# import matplotlib.animation as plt_anim
+from mpl_toolkits.mplot3d import Axes3D
+
+import re
+import inspect
+import imp
+
+import utils
+# import submitpbs
+import load_experimental_data
+
+this_file = inspect.getfile(inspect.currentframe())
+
+parameters_entryscript = dict(
+    action_to_do='launcher_do_reload_constrained_parameters',
+    output_directory='.')
+
+generator_script = 'generator' + re.split("^reloader",
+                                          os.path.split(this_file)[-1])[-1]
+
+print "Reloader data generated from ", generator_script
+
+generator_module = imp.load_source(
+    os.path.splitext(generator_script)[0], generator_script)
+dataset_infos = dict(
+    label='Grid for thesis, showing difference between metrics.',
+    files="%s/%s*.npy" %
+    (generator_module.pbs_submission_infos['simul_out_dir'],
+     generator_module.pbs_submission_infos['other_options']['label'].split(
+         '{')[0]),
+    launcher_module=generator_module,
+    loading_type='args',
+    construct_multidimension_npyarr=False,
+    # limit_max_files=1,
+    parameters=['M', 'ratio_conj', 'sigmax'],
+    variables_to_load=[
+        'result_ll_sum', 'result_ll_n', 'result_bic', 'result_ll90_sum',
+        'result_precision', 'result_ll_median', 'result_em_fits',
+        'result_emfit_mse', 'result_emfit_mixt_kl', 'result_fi_theo',
+        'result_fi_theocov'
+    ],
+    post_processing=None,
+    save_output_filename='metricmismatch_%s_ratiosigmax' %
+    generator_module.experiment_id)
+
+if __name__ == '__main__':
+
+    print "Running ", this_file
+
+    arguments_dict = dict(parameters_filename=this_file)
+    arguments_dict.update(parameters_entryscript)
+    experiment_launcher = ExperimentLauncher(
+        run=True, arguments_dict=arguments_dict)
+
+    variables_to_reinstantiate = [
+        'data_gen', 'sampler', 'stat_meas', 'random_network', 'args',
+        'constrained_parameters', 'data_pbs', 'dataio',
+        'post_processing_outputs', 'fit_exp'
+    ]
+
+    if 'variables_to_save' in experiment_launcher.all_vars:
+        # Also reinstantiate the variables we saved
+        variables_to_reinstantiate.extend(
+            experiment_launcher.all_vars['variables_to_save'])
+
+    for var_reinst in variables_to_reinstantiate:
+        if var_reinst in experiment_launcher.all_vars:
+            vars()[var_reinst] = experiment_launcher.all_vars[var_reinst]
+
+    for var_reinst in post_processing_outputs:
+        vars()[var_reinst] = post_processing_outputs[var_reinst]
