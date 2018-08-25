d522c1312b472a544a172c51695594fd6a574a1d
Loic Matthey <loic.matthey@gatsby.ucl.ac.uk>
2018-08-25 21:17:55 +0100

    Plots for Fisher Info and comparison

---
+++ Experiments/fisherinfo_new_2016/generator_fisher2016_random_Mratio_5try_240818.py
@@ -0,0 +1,118 @@
+import os
+import numpy as np
+import experimentlauncher
+import inspect
+
+# Commit @d8c9acb
+
+# Read from other scripts
+parameters_entryscript = dict(
+    action_to_do='launcher_do_generate_submit_pbs_from_param_files',
+    output_directory='.')
+submit_jobs = True
+
+parameter_generation = 'random'
+num_random_samples = 10000
+limit_max_queued_jobs = 150
+
+resource = ''
+
+# partition = 'wrkstn'
+# partition = 'test'
+partition = 'intel-ivy'
+
+# submit_cmd = 'qsub'
+submit_cmd = 'sbatch'
+
+num_repetitions = 10
+
+run_label = 'fisher2016_random_Mratio_5try_repetitions{num_repetitions}_240818'
+sleeping_period = dict(min=10, max=30)
+
+pbs_submission_infos = dict(
+    description=
+    'New latest 2018 sweep, to get Figure 4.1 and 4.2 for Thesis, showing Fisher info and other metrics comparisons + the 2D plot of FI/kappa ratio for varying M and Ratio_conj.',
+    command='python $WORKDIR/experimentlauncher.py',
+    other_options=dict(
+        action_to_do='launcher_check_fisher_fit_1obj_2016',
+        code_type='mixed',
+        output_directory='.',
+        ratio_conj=1,
+        autoset_parameters=None,
+        M=14 * 14,
+        sigmax=0.25,
+        renormalize_sigma=None,
+        N=300,
+        R=2,
+        T=1,
+        sigmay=0.000001,
+        sigma_output=0.0,
+        sigma_baseline=0.0,
+        inference_method='sample',
+        num_samples=50,
+        selection_num_samples=1,
+        selection_method='last',
+        slice_width=0.07,
+        burn_samples=50,
+        num_repetitions=num_repetitions,
+        # enforce_min_distance=0.17,
+        enforce_min_distance=0.000000001,
+        specific_stimuli_random_centers=None,
+        stimuli_generation='random_fixed_first',
+        stimuli_generation_recall='random',
+        rc_scale=1.0,
+        label=run_label,
+        experiment_data_dir=os.path.normpath(
+            os.path.join(os.environ['WORKDIR_DROP'],
+                         '../../experimental_data')),
+    ),
+    walltime='1:00:00',
+    memory='2gb',
+    simul_out_dir=os.path.join(os.getcwd(), run_label.format(**locals())),
+    pbs_submit_cmd=submit_cmd,
+    limit_max_queued_jobs=limit_max_queued_jobs,
+    source_dir=os.environ['WORKDIR_DROP'],
+    submit_label='fisher_5try_2408',
+    resource=resource,
+    partition=partition,
+    qos='auto')
+
+
+## Define our filtering function
+def filtering_function(new_parameters,
+                       dict_parameters_range,
+                       function_parameters=None):
+  '''
+    Given M, will adapt it to the closest squared value.
+
+    or if should_clamp is False, will not change them
+    '''
+  M_conj_prior = int(new_parameters['M'])
+  M_conj_true = int(np.floor(M_conj_prior**0.5)**2.)
+
+  if function_parameters['should_clamp']:
+    # Clamp them and return true
+    new_parameters['M'] = M_conj_true
+    return True
+  else:
+    return np.allclose(M_conj_true, new_parameters['M'])
+
+
+filtering_function_parameters = {'should_clamp': True}
+
+dict_parameters_range = {
+    'M': dict(sampling_type='randint', low=6, high=625, dtype=int),
+    'ratio_conj': dict(
+        sampling_type='uniform', low=0.01, high=1.0, dtype=float),
+}
+
+if __name__ == '__main__':
+
+  this_file = inspect.getfile(inspect.currentframe())
+  print "Running ", this_file
+
+  arguments_dict = dict(parameters_filename=this_file)
+  arguments_dict.update(parameters_entryscript)
+
+  experiment_launcher = experimentlauncher.ExperimentLauncher(
+      run=True, arguments_dict=arguments_dict)
---
+++ Experiments/fisherinfo_new_2016/reloader_fisher2016_random_Mratio_5try_240818.py
@@ -0,0 +1,71 @@
+"""
+    ExperimentDescriptor for Fitting experiments in a mixed population code
+"""
+
+import os
+
+from experimentlauncher import ExperimentLauncher
+import re
+import inspect
+import imp
+
+this_file = inspect.getfile(inspect.currentframe())
+
+parameters_entryscript = dict(
+    action_to_do='launcher_do_reload_constrained_parameters',
+    output_directory='.')
+
+generator_script = 'generator' + re.split("^reloader",
+                                          os.path.split(this_file)[-1])[-1]
+
+print "Reloader data generated from ", generator_script
+
+generator_module = imp.load_source(
+    os.path.splitext(generator_script)[0], generator_script)
+dataset_infos = dict(
+    label=
+    'New latest 2018 sweep, to get Figure 4.1 and 4.2 for Thesis, showing Fisher info and other metrics comparisons + the 2D plot of FI/kappa ratio for varying M and Ratio_conj.',
+    files="%s/%s*.npy" %
+    (generator_module.pbs_submission_infos['simul_out_dir'],
+     generator_module.pbs_submission_infos['other_options']['label'].split(
+         '{')[0]),
+    launcher_module=generator_module,
+    loading_type='args',
+    construct_multidimension_npyarr=False,
+    # limit_max_files=1,
+    parameters=['M', 'ratio_conj'],
+    variables_to_load=[
+        'result_all_precisions', 'result_FI_rc_curv', 'result_FI_rc_theo',
+        'result_FI_rc_theocov', 'result_FI_rc_theo_circulant',
+        'result_FI_rc_theo_largeN', 'result_marginal_inv_FI',
+        'result_marginal_FI', 'result_em_fits'
+    ],
+    post_processing=None,
+    save_output_filename='mratio_rnd')
+
+if __name__ == '__main__':
+
+  print "Running ", this_file
+
+  arguments_dict = dict(parameters_filename=this_file)
+  arguments_dict.update(parameters_entryscript)
+  experiment_launcher = ExperimentLauncher(
+      run=True, arguments_dict=arguments_dict)
+
+  variables_to_reinstantiate = [
+      'data_gen', 'sampler', 'stat_meas', 'random_network', 'args',
+      'constrained_parameters', 'data_pbs', 'dataio',
+      'post_processing_outputs', 'fit_exp'
+  ]
+
+  if 'variables_to_save' in experiment_launcher.all_vars:
+    # Also reinstantiate the variables we saved
+    variables_to_reinstantiate.extend(
+        experiment_launcher.all_vars['variables_to_save'])
+
+  for var_reinst in variables_to_reinstantiate:
+    if var_reinst in experiment_launcher.all_vars:
+      vars()[var_reinst] = experiment_launcher.all_vars[var_reinst]
+
+  for var_reinst in post_processing_outputs:
+    vars()[var_reinst] = post_processing_outputs[var_reinst]
